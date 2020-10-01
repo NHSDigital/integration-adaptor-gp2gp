3afdac0ac842a612af644efc15435a620515deb0
Bartek Sarul <b.sarul@kainos.com>
2020-10-01 13:39:15 +0200

    Stub RCMR_IN030000UK06 response

--- src/main/java/uk/nhs/adaptors/gp2gp/services/FhirToHL7TranslationService.java
+++ src/main/java/uk/nhs/adaptors/gp2gp/services/FhirToHL7TranslationService.java
@@ -1,11 +1,16 @@
 package uk.nhs.adaptors.gp2gp.services;
 
+import com.github.mustachejava.Mustache;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.springframework.stereotype.Component;
+import uk.nhs.adaptors.gp2gp.utils.TemplateUtils;
 
 @Component
 public class FhirToHL7TranslationService {
+
+    private static final Mustache RCMR_IN030000UK06_TEMPLATE = TemplateUtils.loadTemplate("RCMR_IN030000UK06.mustache");
+
     public String translate(Bundle gpConnectResponse) {
-        return "<root/>";
+        return TemplateUtils.fillTemplate(RCMR_IN030000UK06_TEMPLATE, new Object());
     }
 }
