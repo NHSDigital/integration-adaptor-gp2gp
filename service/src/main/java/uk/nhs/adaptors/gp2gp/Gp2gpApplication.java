41e009c3f9e26d32d9029915872a8ae8291ba322
Broom94 <42891767+Broom94@users.noreply.github.com>
2020-11-24 17:51:09 +0000

    spring app and docker

---
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/Gp2gpApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.adaptors.gp2gp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gp2gpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gp2gpApplication.class, args);
+	}
+
+}
