9dc4f3d5b791d2c19b04041dcec860c9223f7109
Matt Dooner <50910155+mattd-kainos@users.noreply.github.com>
2020-12-03 15:40:58 +0000

    Adds a unit test and jacoco (#11)

---
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/Gp2gpApplicationTest.java
@@ -0,0 +1,13 @@
+package uk.nhs.adaptors.gp2gp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gp2gpApplicationTest {
+
+    @Test
+    void whenUsingApplicationTestContextThenApplicationCanStart() {
+    }
+
+}
