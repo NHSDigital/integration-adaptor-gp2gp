28c1ac8756dfa344abfc43827783ef8614270562
Matt Dooner <mattd@kainos.com>
2020-12-09 19:04:59 +0000

    * build.gradle changes so integrationTests count towards coverage * created a temp unit test so something appears in the junit report * gave the integration tests container a name - maybe we can run all tests at once and save some time?

---
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/TemporaryTest.java
@@ -0,0 +1,11 @@
+package uk.nhs.adaptors.gp2gp;
+
+import org.junit.jupiter.api.Test;
+
+class TemporaryTest {
+
+    @Test
+    void whenPipelineRunsExpectAtLeastOneTestInTestReport() {
+    }
+
+}
