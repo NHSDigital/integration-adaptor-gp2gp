ef6302bf90937f4003a2e7f071b76f2308bbe917
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-12-09 13:25:50 +0100

    Implemented test fixes

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/common/storageconnectors/StorageConnectorTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/common/storageconnectors/StorageConnectorTest.java
@@ -3,16 +3,14 @@ package uk.nhs.adaptors.gp2gp.common.storageconnectors;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import uk.nhs.adaptors.gp2gp.extension.IntegrationTestsExtension;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.After;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,31 +24,33 @@ public class StorageConnectorTest {
     @Autowired
     private StorageConnector storageConnector;
 
-    private static final String FILE_EXPECTED_DOWNLOAD = "src/intTest/resources/downloads/test.txt";
     private static final String SETUP_FILE_TXT = "setupFile.txt";
-    private File uploadedFile;
-    private File downloadedFile;
+    private byte[] uploadedFileBytes;
+    private InputStream downloadedFileStream;
 
     @BeforeEach
     public void setupConnector() throws IOException {
         String fileToUpload = StorageConnectorTest.class.getResource("/test.txt").getPath();
-        uploadedFile = new File(fileToUpload);
-        InputStream inputStream = FileUtils.openInputStream(uploadedFile);
-        storageConnector.uploadToStorage(inputStream, inputStream.available(), SETUP_FILE_TXT);
+        File uploadedFile = new File(fileToUpload);
+
+        InputStream tempUploadedFileStream = FileUtils.openInputStream(uploadedFile);
+        uploadedFileBytes = tempUploadedFileStream.readAllBytes();
+        tempUploadedFileStream.close();
+
+        InputStream uploadedFileStream = FileUtils.openInputStream(uploadedFile);
+        storageConnector.uploadToStorage(uploadedFileStream, uploadedFileStream.available(), SETUP_FILE_TXT);
+        uploadedFileStream.close();
     }
 
     @Test
     public void When_DownloadingFileFromStorage_Expect_FileContentToMatch() throws IOException {
-        InputStream inputStream = new ByteArrayInputStream(storageConnector.downloadFromStorage(SETUP_FILE_TXT).readAllBytes());
-        downloadedFile = new File(FILE_EXPECTED_DOWNLOAD);
-        FileUtils.copyToFile(inputStream, downloadedFile);
+        downloadedFileStream = storageConnector.downloadFromStorage(SETUP_FILE_TXT);
 
-        assertThat(FileUtils.contentEquals(uploadedFile, downloadedFile), is(true));
+        assertThat(downloadedFileStream.readAllBytes(), is(uploadedFileBytes));
     }
 
-    @AfterEach
-    @SuppressFBWarnings("RV_RETURN_VALUE_IGNORED_BAD_PRACTICE")
-    public void deleteDownloadedFile() {
-        downloadedFile.delete();
+    @After
+    public void tearDown() throws IOException {
+        downloadedFileStream.close();
     }
 }
