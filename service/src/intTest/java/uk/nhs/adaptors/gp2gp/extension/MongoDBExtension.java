b89ee17985088ac5eea0925060194f69b603df2b
Matt Dooner <50910155+mattd-kainos@users.noreply.github.com>
2020-12-11 13:37:28 +0000

    NIAD-764 Pipeline: Restore project structure, unit test reports, run tests in single stage (#18)
    Restore docker/ project structure, unit test report, run tests in siingle stage
    * Adds a DISABLE_TEST_CONTAINERS environment variable for integration tests
    
    * * Added missing 'docker build -f' to Build Docker Images stage
    
    * Added junit task to publish test report
    
    * Added find . to debug dir structure
    
    * build.gradle changes so integrationTests count towards coverage
    * created a temp unit test so something appears in the junit report
    * gave the integration tests container a name - maybe we can run all tests at once and save some time?
    
    * all tests in a single step
    
    * only run tests once
    
    * fix dockerfile error
    
    * docker-compose-tests.yml is now an override of the main docker-compose instead of a copy
    Dockerfile uses bare gradle command to avoid downloading wrapper
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * removed version from build.grade - we version the docker images
    
    * less severe docker image cleanup
    
    * escape single quote
    
    * Update Jenkinsfile
    
    * moved activemq into docker/
    
    * restored previous image delete cleanup command
    
    * * testcontainer optional for amq
    * gp2gp dependes on amq in docker-compose
    * Jenkinsfile cleanup
    
    * constants, README
    
    * removed comment
    
    * removed constant

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/extension/MongoDBExtension.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/extension/MongoDBExtension.java
@@ -10,11 +10,15 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static uk.nhs.adaptors.gp2gp.TestContainerUtils.isTestContainersEnabled;
+
 @Slf4j
 public class MongoDBExtension implements BeforeAllCallback, BeforeEachCallback {
     @Override
     public void beforeAll(ExtensionContext context) {
-        MongoDbContainer.getInstance().start();
+        if (isTestContainersEnabled()) {
+            MongoDbContainer.getInstance().start();
+        }
     }
 
     @Override
