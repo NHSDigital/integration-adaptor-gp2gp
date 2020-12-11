ca407d0c2ddfc7a25812da40ae8895cf3068efac
Matt Dooner <mattd@kainos.com>
2020-12-11 08:25:47 +0000

    constants, README

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/TestContainerUtils.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/TestContainerUtils.java
@@ -1,7 +1,11 @@
 package uk.nhs.adaptors.gp2gp;
 
 public class TestContainerUtils {
+
+    public static final String TRUE = "true";
+    public static final String DISABLE_TEST_CONTAINERS = "DISABLE_TEST_CONTAINERS";
+
     public static boolean isTestContainersEnabled() {
-        return !"true".equalsIgnoreCase(System.getenv("DISABLE_TEST_CONTAINERS"));
+        return !TRUE.equalsIgnoreCase(System.getenv(DISABLE_TEST_CONTAINERS));
     }
 }
