37c2e9b1d4de898eb521487055a31539c07ea769
Matt Dooner <mattd@kainos.com>
2020-12-11 08:35:01 +0000

    removed constant

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/TestContainerUtils.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/TestContainerUtils.java
@@ -2,10 +2,9 @@ package uk.nhs.adaptors.gp2gp;
 
 public class TestContainerUtils {
 
-    public static final String TRUE = "true";
     public static final String DISABLE_TEST_CONTAINERS = "DISABLE_TEST_CONTAINERS";
 
     public static boolean isTestContainersEnabled() {
-        return !TRUE.equalsIgnoreCase(System.getenv(DISABLE_TEST_CONTAINERS));
+        return !Boolean.TRUE.toString().equalsIgnoreCase(System.getenv(DISABLE_TEST_CONTAINERS));
     }
 }
