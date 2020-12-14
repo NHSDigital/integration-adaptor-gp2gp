ce4c3f4af350c90d4d33ff60798ab6ca66a3725a
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-12-14 10:55:47 +0100

    Implemented CR changes

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/EhrExtractStatusRepositoryTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/EhrExtractStatusRepositoryTest.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 import java.util.Optional;
 
 import uk.nhs.adaptors.gp2gp.configurations.MongoClientConfiguration;
+import uk.nhs.adaptors.gp2gp.constants.EhrStatusConstants;
 import uk.nhs.adaptors.gp2gp.extension.ActiveMQExtension;
 import uk.nhs.adaptors.gp2gp.extension.MongoDBExtension;
 import uk.nhs.adaptors.gp2gp.models.EhrExtractStatus;
@@ -22,16 +23,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith({ SpringExtension.class, MongoDBExtension.class, ActiveMQExtension.class})
 @SpringBootTest
 public class EhrExtractStatusRepositoryTest {
-    private static final String EXTRACT_ID = "test-extract-id";
-    private static final String REQUEST_ID = "041CA2AE-3EC6-4AC9-942F-0F6621CC0BFC";
-    private static final String CONVERSATION_ID = "DFF5321C-C6EA-468E-BBC2-B0E48000E071";
-    private static final String NHS_NUMBER = "9692294935";
-    private static final String FROM_PARTY_ID = "N82668-820670";
-    private static final String TO_PARTY_ID = "B86041-822103";
-    private static final String FROM_ASID = "200000000205";
-    private static final String TO_ASID = "200000001161";
-    private static final String FROM_ODS_CODE = "N82668";
-    private static final String TO_ODS_CODE = "B86041";
 
     @Autowired
     private MongoClientConfiguration mongoClientConfiguration;
@@ -41,26 +32,26 @@ public class EhrExtractStatusRepositoryTest {
     @Test
     public void When_AddingNewEhrExtractStatus_Expect_EhrExtractStatusRetrievableByIdFromDatabase() {
         Instant now = Instant.now();
-        ehrExtractStatusRepository.save(new EhrExtractStatus(EXTRACT_ID,
+        ehrExtractStatusRepository.save(new EhrExtractStatus(EhrStatusConstants.EXTRACT_ID,
             now,
             now,
-            CONVERSATION_ID,
-            new EhrExtractStatus.EhrRequest(REQUEST_ID,
-                NHS_NUMBER,
-                FROM_PARTY_ID,
-                TO_PARTY_ID,
-                FROM_ASID,
-                TO_ASID,
-                FROM_ODS_CODE,
-                TO_ODS_CODE)
+            EhrStatusConstants.CONVERSATION_ID,
+            new EhrExtractStatus.EhrRequest(EhrStatusConstants.REQUEST_ID,
+                EhrStatusConstants.NHS_NUMBER,
+                EhrStatusConstants.FROM_PARTY_ID,
+                EhrStatusConstants.TO_PARTY_ID,
+                EhrStatusConstants.FROM_ASID,
+                EhrStatusConstants.TO_ASID,
+                EhrStatusConstants.FROM_ODS_CODE,
+                EhrStatusConstants.TO_ODS_CODE)
         ));
-        Optional<EhrExtractStatus> optionalEhrExtractStatus = ehrExtractStatusRepository.findById(EXTRACT_ID);
+        Optional<EhrExtractStatus> optionalEhrExtractStatus = ehrExtractStatusRepository.findById(EhrStatusConstants.EXTRACT_ID);
 
         assertThat(optionalEhrExtractStatus.isPresent(), is(true));
 
         EhrExtractStatus ehrExtractStatus = optionalEhrExtractStatus.get();
 
-        assertThat(ehrExtractStatus.getExtractId(), is(EXTRACT_ID));
+        assertThat(ehrExtractStatus.getExtractId(), is(EhrStatusConstants.EXTRACT_ID));
         assertThat(ehrExtractStatus.getCreated(), is(notNullValue()));
     }
 }
--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/MessageQueueTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/MessageQueueTest.java
@@ -13,6 +13,7 @@ import java.util.Iterator;
 import javax.jms.JMSException;
 import javax.jms.Message;
 
+import uk.nhs.adaptors.gp2gp.constants.EhrStatusConstants;
 import uk.nhs.adaptors.gp2gp.extension.ActiveMQExtension;
 import uk.nhs.adaptors.gp2gp.extension.MongoDBExtension;
 import uk.nhs.adaptors.gp2gp.models.EhrExtractStatus;
@@ -32,15 +33,6 @@ public class MessageQueueTest {
     private static final String INVALID_MESSAGE_CONTENT = "TRASH";
     private static final String DLQ = "ActiveMQ.DLQ";
     private static final String EHR_EXTRACT_REQUEST_TEST_FILE = "/ehrExtractRequest.json";
-    private static final String CONVERSATION_ID = "DFF5321C-C6EA-468E-BBC2-B0E48000E071";
-    private static final String REQUEST_ID = "041CA2AE-3EC6-4AC9-942F-0F6621CC0BFC";
-    private static final String NHS_NUMBER = "9692294935";
-    private static final String FROM_PARTY_ID = "N82668-820670";
-    private static final String TO_PARTY_ID = "B86041-822103";
-    private static final String FROM_ASID = "200000000205";
-    private static final String TO_ASID = "200000001161";
-    private static final String FROM_ODS_CODE = "N82668";
-    private static final String TO_ODS_CODE = "B86041";
     private static final long TIMEOUT = 2000;
     private static final long EXPECTED_COUNT = 1;
 
@@ -75,19 +67,19 @@ public class MessageQueueTest {
         assertThat(ehrExtractStatus.getExtractId(), is(notNullValue()));
         assertThat(ehrExtractStatus.getConversationId(), is(notNullValue()));
         assertThat(ehrExtractStatus.getConversationId(), is(notNullValue()));
-        assertThat(ehrExtractStatus.getConversationId(), is(CONVERSATION_ID));
+        assertThat(ehrExtractStatus.getConversationId(), is(EhrStatusConstants.CONVERSATION_ID));
 
         EhrExtractStatus.EhrRequest ehrRequest = ehrExtractStatus.getEhrRequest();
 
         assertThat(ehrRequest, is(notNullValue()));
-        assertThat(ehrRequest.getRequestId(), is(REQUEST_ID));
-        assertThat(ehrRequest.getNhsNumber(), is(NHS_NUMBER));
-        assertThat(ehrRequest.getFromPartyId(), is(FROM_PARTY_ID));
-        assertThat(ehrRequest.getToPartyId(), is(TO_PARTY_ID));
-        assertThat(ehrRequest.getFromAsid(), is(FROM_ASID));
-        assertThat(ehrRequest.getToAsid(), is(TO_ASID));
-        assertThat(ehrRequest.getFromOdsCode(), is(FROM_ODS_CODE));
-        assertThat(ehrRequest.getToOdsCode(), is(TO_ODS_CODE));
+        assertThat(ehrRequest.getRequestId(), is(EhrStatusConstants.REQUEST_ID));
+        assertThat(ehrRequest.getNhsNumber(), is(EhrStatusConstants.NHS_NUMBER));
+        assertThat(ehrRequest.getFromPartyId(), is(EhrStatusConstants.FROM_PARTY_ID));
+        assertThat(ehrRequest.getToPartyId(), is(EhrStatusConstants.TO_PARTY_ID));
+        assertThat(ehrRequest.getFromAsid(), is(EhrStatusConstants.FROM_ASID));
+        assertThat(ehrRequest.getToAsid(), is(EhrStatusConstants.TO_ASID));
+        assertThat(ehrRequest.getFromOdsCode(), is(EhrStatusConstants.FROM_ODS_CODE));
+        assertThat(ehrRequest.getToOdsCode(), is(EhrStatusConstants.TO_ODS_CODE));
     }
 
     @Test
