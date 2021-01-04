310356b489d5eff61bc36057d1557e8641ac634a
Matt Dooner <mattd@kainos.com>
2021-01-04 08:29:56 +0000

    remove comments and checkstyle

--- e2e-tests/src/main/java/uk/nhs/adaptors/gp2gp/MessageQueue.java
+++ e2e-tests/src/main/java/uk/nhs/adaptors/gp2gp/MessageQueue.java
@@ -1,6 +1,11 @@
 package uk.nhs.adaptors.gp2gp;
 
-import javax.jms.*;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import java.util.Hashtable;
@@ -11,7 +16,6 @@ public class MessageQueue {
         String broker = System.getenv().getOrDefault("GP2GP_AMQP_BROKERS", "amqp://localhost:5672");
         String queueName = System.getenv().getOrDefault("GP2GP_MHS_INBOUND_QUEUE", "inbound");
 
-        // adapted from https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-java-how-to-use-jms-api-amqp
         Hashtable<String, String> hashtable = new Hashtable<>();
         hashtable.put("connectionfactory.SBCF", broker);
         hashtable.put("queue.QUEUE", queueName);
@@ -19,24 +23,15 @@ public class MessageQueue {
         Context context = new InitialContext(hashtable);
 
         ConnectionFactory cf = (ConnectionFactory) context.lookup("SBCF");
-
-        // Look up queue
         Destination queue = (Destination) context.lookup("QUEUE");
-
-        // Create connection
-        Connection connection = cf.createConnection(); // Azure uses params (csb.getSasKeyName(), csb.getSasKey())
-        // Create session, no transaction, client ack
+        Connection connection = cf.createConnection();
         Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
 
-        // Create producer
         MessageProducer producer = session.createProducer(queue);
-
-        // Send message
         TextMessage message = session.createTextMessage();
         message.setText(messageContent);
         producer.send(message);
 
-        // should be closed in a finally
         producer.close();
         session.close();
         connection.stop();
