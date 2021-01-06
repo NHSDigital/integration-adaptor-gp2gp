7d30c66fb3203148d031097e8304a87353cc2116
Matt Dooner <mattd@kainos.com>
2021-01-06 16:16:22 +0000

    mongo truncates to milliseconds

--- service/src/main/java/uk/nhs/adaptors/gp2gp/common/service/TimestampService.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/common/service/TimestampService.java
@@ -3,10 +3,11 @@ package uk.nhs.adaptors.gp2gp.common.service;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Service
 public class TimestampService {
     public Instant now() {
-        return Instant.now();
+        return Instant.now().truncatedTo(ChronoUnit.MILLIS);
     }
 }
