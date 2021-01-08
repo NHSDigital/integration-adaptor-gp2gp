119cdcca1503cad520dab0155173a92d3bed66db
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-01-08 14:15:31 +0000

    NIAD - 761: Fix object mapper error and add component to executors (#31)
    * fix object mapper error and add component to executors
    
    * remove data annotation
    
    * remove unused imports

--- service/src/main/java/uk/nhs/adaptors/gp2gp/gpc/GetGpcDocumentTaskExecutor.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/gpc/GetGpcDocumentTaskExecutor.java
@@ -1,9 +1,12 @@
 package uk.nhs.adaptors.gp2gp.gpc;
 
+import org.springframework.stereotype.Component;
+
 import lombok.extern.slf4j.Slf4j;
 import uk.nhs.adaptors.gp2gp.common.task.TaskExecutor;
 
 @Slf4j
+@Component
 public class GetGpcDocumentTaskExecutor implements TaskExecutor<GetGpcDocumentTaskDefinition> {
 
     @Override
