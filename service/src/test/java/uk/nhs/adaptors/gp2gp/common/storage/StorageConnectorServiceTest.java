09a661d5532a3a6bee1e7a76fb87c17b408bba9c
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-01-15 15:55:53 +0000

    storage test refactor

--- service/src/test/java/uk/nhs/adaptors/gp2gp/common/storage/StorageConnectorServiceTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/common/storage/StorageConnectorServiceTest.java
@@ -1,13 +1,14 @@
 package uk.nhs.adaptors.gp2gp.common.storage;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import static uk.nhs.adaptors.gp2gp.gpc.GpcFileNameConstants.GPC_STRUCTURED_FILE_EXTENSION;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
@@ -16,43 +17,38 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.SneakyThrows;
+
 @ExtendWith(MockitoExtension.class)
 public class StorageConnectorServiceTest {
-    private static final String RESPONSE_STRING = "£$%^^&£$abc";
-    private static final long EXPECTED_INPUT_LENGTH = 13;
-    private static final InputStream EXPECTED_INPUT_STREAM = new ByteArrayInputStream(RESPONSE_STRING.getBytes());
-    @Captor
-    private ArgumentCaptor<InputStream> actualInputStream;
-    @Captor
-    private ArgumentCaptor<Long> actualInputLength;
-    @Captor
-    private ArgumentCaptor<String> actualFileName;
-    @Mock
-    private StorageDataWrapper storageDataWrapper;
+    private static final long EXPECTED_STREAM_LENGTH = 8;
     @Mock
     private StorageConnector storageConnector;
     @InjectMocks
     private StorageConnectorService storageConnectorService;
     @Mock
     private ObjectMapper objectMapper;
+    @Mock
+    private StorageDataWrapper anyStorageDataWrapper;
+
+    @Captor
+    private ArgumentCaptor<InputStream> inputStreamArgumentCaptor;
 
     @Test
-    public void When_ValidStorageDataWrapperIsPass_Expect_UploadStoragePramsHaveCorrectValues() throws IOException {
-        var conversationId = "conversationId";
-        when(storageDataWrapper.getConversationId()).thenReturn(conversationId);
-        when(objectMapper.writeValueAsString(any())).thenReturn(RESPONSE_STRING);
-
-        storageConnectorService.uploadWithMetadata(storageDataWrapper);
-        Mockito.verify(storageConnector).uploadToStorage(actualInputStream.capture(),
-            actualInputLength.capture(), actualFileName.capture());
-
-        assertThat(actualFileName.getValue()).isEqualTo(conversationId + GPC_STRUCTURED_FILE_EXTENSION);
-        assertThat(actualInputLength.getValue()).isEqualTo(EXPECTED_INPUT_LENGTH);
-        assertThat(actualInputStream.getValue().readAllBytes()).isEqualTo(EXPECTED_INPUT_STREAM.readAllBytes());
+    @SneakyThrows
+    public void When_ValidStorageDataWrapperIsPass_Expect_UploadStoragePramsHaveCorrectValues() {
+        when(objectMapper.writeValueAsString(anyStorageDataWrapper)).thenReturn("response");
+        when(anyStorageDataWrapper.getConversationId()).thenReturn("UUID");
+        storageConnectorService.uploadWithMetadata(anyStorageDataWrapper);
+        verify(storageConnector).uploadToStorage(
+            inputStreamArgumentCaptor.capture(),
+            eq(EXPECTED_STREAM_LENGTH),
+            eq("UUID" + GPC_STRUCTURED_FILE_EXTENSION));
+        var actualContent = new String(inputStreamArgumentCaptor.getValue().readAllBytes(), UTF_8);
+        assertThat(actualContent).isEqualTo("response");
     }
 }
