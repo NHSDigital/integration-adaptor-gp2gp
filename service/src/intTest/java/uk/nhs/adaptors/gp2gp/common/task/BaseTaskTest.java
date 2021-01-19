4a56f0094e312d09e871f6376d55d2d994c0009a
Matt Dooner <mattd@kainos.com>
2021-01-19 10:27:06 +0000

    mockbean

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/common/task/BaseTaskTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/common/task/BaseTaskTest.java
@@ -3,6 +3,7 @@ package uk.nhs.adaptors.gp2gp.common.task;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.nhs.adaptors.gp2gp.testcontainers.ActiveMQExtension;
@@ -13,8 +14,8 @@ import uk.nhs.adaptors.gp2gp.testcontainers.MongoDBExtension;
 @DirtiesContext
 @SuppressWarnings("checkstyle:VisibilityModifier")
 public abstract class BaseTaskTest {
-    @Mock
+    @MockBean
     protected TaskDispatcher taskDispatcher;
-    @Mock
+    @MockBean
     protected TaskConsumer taskConsumer;
 }
