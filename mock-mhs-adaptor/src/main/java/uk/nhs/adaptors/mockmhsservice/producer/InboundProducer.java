75e3f3854c25cc37159d131e8df8363869ab4b83
z-uddin <74315663+z-uddin@users.noreply.github.com>
2021-01-20 09:12:46 +0000

    NIAD-773 Mock MHS Adaptor  (#41)
    * Initial mock of MHS adaptor
    
    * Initial WIP Spike
    
    * Refactoring InboundProducer to use JMStemplate and adding try catch block to mock controller
    
    * WIP parsing json body from request
    
    * Refactored to use ObjectMapper to send JSON message response. Parse request JSON to match expected payload JSON, fail otherwise.
    
    * Send back 500 when Interaction-Id header is not sent in request. Added response JSON object mapper code to and made Interaction-Id an optional header in service.
    
    * Added stub xml file from ticket. Now on valid request body with appropriate Interaction-Id header, produces inbound reply using stub xml as the data content.
    
    * Refactored exception handling messages to be more descriptive. Added missing request headers to match MHS Outbound API. Refactored imports and some variable names.
    
    * Added custom mock MHS exception.
    
    * Refactored code to reduce number of try/catch blocks.
    
    * Uncommenting out docker image for mock adaptor. Refactoring mock service.
    
    * WIP fixing resource file reference
    
    * get resource
    
    * Refactoring get resource references.
    
    * Fixed docker image for mock MHS adaptor. Added docker compose down to start script.
    
    * refactor
    
    * alter dockerfie for correct jar name
    
    * Removed redundant line from .gitignore
    
    * Initial changes from PR review.
    
    * Addressed PR review comments
    
    * Further PR review comments addressed
    
    * Added README. Added resource files as stub response bodies based off MHS service response via OpenTest.
    
    * Updated README
    
    Co-authored-by: Matt Dooner <mattd@kainos.com>
    Co-authored-by: Broom94 <42891767+Broom94@users.noreply.github.com>

---
+++ mock-mhs-adaptor/src/main/java/uk/nhs/adaptors/mockmhsservice/producer/InboundProducer.java
@@ -0,0 +1,21 @@
+package uk.nhs.adaptors.mockmhsservice.producer;
+
+import org.springframework.jms.JmsException;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class InboundProducer {
+
+    private final JmsTemplate jmsTemplate;
+    @Value("${gp2gp.amqp.inboundQueueName}")
+    private String queueName;
+
+    public void sendToMhsInboundQueue(String messageContent) throws JmsException {
+        jmsTemplate.send(queueName, session -> session.createTextMessage(messageContent));
+    }
+}
