fceae417988967d44edb9d6a996dee627ccd8788
Zakariyya Uddin <z.uddin@kainos.com>
2021-01-26 16:52:40 +0000

    Refactored invalid component test to use mocks

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/SendEhrExtractCoreComponentTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/SendEhrExtractCoreComponentTest.java
@@ -11,21 +11,33 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
 import uk.nhs.adaptors.gp2gp.common.task.BaseTaskTest;
-import uk.nhs.adaptors.gp2gp.mhs.InvalidOutboundMessageException;
+import uk.nhs.adaptors.gp2gp.mhs.MhsClient;
 import uk.nhs.adaptors.gp2gp.testcontainers.ActiveMQExtension;
 import uk.nhs.adaptors.gp2gp.testcontainers.MongoDBExtension;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @RunWith(SpringRunner.class)
-@ExtendWith({SpringExtension.class, MongoDBExtension.class, ActiveMQExtension.class})
+@ExtendWith({SpringExtension.class, MongoDBExtension.class, ActiveMQExtension.class, MockitoExtension.class})
 @SpringBootTest
 @DirtiesContext
 public class SendEhrExtractCoreComponentTest extends BaseTaskTest {
+    @Mock
+    private MhsClient mhsClient;
+    @Mock
+    private WebClient.RequestHeadersSpec<?> request;
+    @Mock
+    private SendEhrExtractCoreTaskDefinition sendEhrExtractCoreTaskDefinition;
 
     @Autowired
+    @InjectMocks
     private SendEhrExtractCoreTaskExecutor sendEhrExtractCoreTaskExecutor;
     @Autowired
     private EhrExtractStatusRepository ehrExtractStatusRepository;
@@ -42,7 +54,16 @@ public class SendEhrExtractCoreComponentTest extends BaseTaskTest {
         assertThatInitialRecordWasUpdated(ehrExtractUpdated, ehrExtractStatus);
     }
 
+    @Test
+    public void When_ExtractCoreThrowsException_Expect_EhrExtractStatusNotUpdated() {
+        var ehrExtractStatus = EhrExtractStatusTestUtils.prepareEhrExtractStatus();
+        ehrExtractStatusRepository.save(ehrExtractStatus);
 
+        assertThrows(EhrExtractException.class, () -> sendEhrExtractCoreTaskExecutor.execute(sendEhrExtractCoreTaskDefinition));
+
+        var ehrExtractUpdated = ehrExtractStatusRepository.findByConversationId(ehrExtractStatus.getConversationId()).get();
+        assertThat(ehrExtractUpdated.getEhrExtractCore()).isNull();
+    }
 
     private SendEhrExtractCoreTaskDefinition buildValidExtractCoreTask(EhrExtractStatus ehrExtractStatus) {
         return SendEhrExtractCoreTaskDefinition.builder()
@@ -60,39 +81,5 @@ public class SendEhrExtractCoreComponentTest extends BaseTaskTest {
         var gpcAccessStructured = ehrExtractStatusUpdated.getEhrExtractCore();
         assertThat(gpcAccessStructured.getSentAt()).isNotNull();
         assertThat(gpcAccessStructured.getTaskId()).isNotNull();
-    }
-}
-
-@RunWith(SpringRunner.class)
-@ExtendWith({SpringExtension.class, MongoDBExtension.class, ActiveMQExtension.class})
-@SpringBootTest(properties = "gp2gp.mhs.url=http://127.0.0.1:8081/invalid-mhs-endpoint")
-@DirtiesContext
-class InvalidSendEhrExtractCoreComponentTest extends BaseTaskTest {
-    @Autowired
-    private SendEhrExtractCoreTaskExecutor sendEhrExtractCoreTaskExecutor;
-    @Autowired
-    private EhrExtractStatusRepository ehrExtractStatusRepository;
-
-    @Test
-    public void When_InvalidMhsOutboundResponse_Expect_EhrExtractStatusNotUpdated() {
-        var ehrExtractStatus = EhrExtractStatusTestUtils.prepareEhrExtractStatus();
-        ehrExtractStatusRepository.save(ehrExtractStatus);
-
-        SendEhrExtractCoreTaskDefinition sendEhrExtractCoreTaskDefinition = buildValidExtractCoreTask(ehrExtractStatus);
-        assertThrows(InvalidOutboundMessageException.class, () -> sendEhrExtractCoreTaskExecutor.execute(sendEhrExtractCoreTaskDefinition));
-
-        var ehrExtractUpdated = ehrExtractStatusRepository.findByConversationId(ehrExtractStatus.getConversationId()).get();
-        assertThat(ehrExtractUpdated.getEhrExtractCore()).isNull();
-    }
-
-    private SendEhrExtractCoreTaskDefinition buildValidExtractCoreTask(EhrExtractStatus ehrExtractStatus) {
-        return SendEhrExtractCoreTaskDefinition.builder()
-            .fromAsid(ehrExtractStatus.getEhrRequest().getFromAsid())
-            .toAsid(ehrExtractStatus.getEhrRequest().getToAsid())
-            .fromOdsCode(ehrExtractStatus.getEhrRequest().getFromOdsCode())
-            .conversationId(ehrExtractStatus.getConversationId())
-            .requestId(ehrExtractStatus.getEhrRequest().getRequestId())
-            .taskId(UUID.randomUUID().toString())
-            .build();
     }
 }
