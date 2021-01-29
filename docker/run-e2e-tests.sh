e63d3c04442ca0ebb203730c20b09b66ac5d5e7d
Matt Dooner <mattd@kainos.com>
2021-01-29 16:58:56 +0000

    script update

--- docker/run-e2e-tests.sh
+++ docker/run-e2e-tests.sh
@@ -5,9 +5,8 @@ set -e
 if [ -f "vars.sh" ]; then
     source vars.sh
 else
-  echo "No vars.sh define. Using docker-compose defaults."
+  echo "No vars.sh defined. Using docker-compose defaults."
 fi
 
-docker-compose down
-docker-compose build
-docker-compose up
+docker-compose -f docker-compose.yml -f docker-compose-e2e-tests.yml build gp2gp-e2e-tests
+docker-compose -f docker-compose.yml -f docker-compose-e2e-tests.yml up --exit-code-from gp2gp-e2e-tests gp2gp-e2e-tests
