42df68d3067816bbb6d98ed5bf16ab833fcd5f38
msolecka <m.solecka@kainos.com>
2021-02-02 10:04:38 +0100

    NIAD-981: Test refactor

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/OutputMessageWrapperMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/OutputMessageWrapperMapperTest.java
@@ -17,11 +17,15 @@ import uk.nhs.adaptors.gp2gp.common.service.TimestampService;
 import uk.nhs.adaptors.gp2gp.gpc.GetGpcDocumentTaskDefinition;
 import uk.nhs.adaptors.gp2gp.utils.ResourceTestFileUtils;
 
-
 @ExtendWith(MockitoExtension.class)
 public class OutputMessageWrapperMapperTest {
     private static final String TEST_ID = "394559384658936";
     private static final String TEST_DATE_TIME = "2020-02-18T17:09:46.01Z";
+    private static final String EXPECTED_OUTPUT_MESSAGE_WRAPPER_XML = "/ehr/mapper/expected-output-message-wrapper.xml";
+    private static final String TRANSFORMED_EXTRACT = "<EhrExtract classCode=\"EXTRACT\" moodCode=\"EVN\"><id "
+        + "root=\"12345\"/></EhrExtract>";
+    private static final String TO_ASID_VALUE = "to-asid-value";
+    private static final String FROM_ASID_VALUE = "from-asid-value";
 
     @Mock
     private RandomIdGeneratorService randomIdGeneratorService;
@@ -29,27 +33,28 @@ public class OutputMessageWrapperMapperTest {
     private TimestampService timestampService;
 
     private OutputMessageWrapperMapper outputMessageWrapperMapper;
+    private GetGpcDocumentTaskDefinition getGpcStructuredTaskDefinition;
+    private CharSequence expectedOutputMessage;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         when(randomIdGeneratorService.createNewId()).thenReturn(TEST_ID);
         when(timestampService.now()).thenReturn(Instant.parse(TEST_DATE_TIME));
         outputMessageWrapperMapper = new OutputMessageWrapperMapper(randomIdGeneratorService, timestampService);
+
+        getGpcStructuredTaskDefinition = GetGpcDocumentTaskDefinition.builder()
+            .toAsid(TO_ASID_VALUE)
+            .fromAsid(FROM_ASID_VALUE)
+            .build();
+
+        expectedOutputMessage = ResourceTestFileUtils.getFileContent(EXPECTED_OUTPUT_MESSAGE_WRAPPER_XML);
     }
 
     @Test
-    public void When_MappingOutputMessageWrapperWithStringContent_Expect_ProperXmlOutput() throws IOException {
-        GetGpcDocumentTaskDefinition getGpcStructuredTaskDefinition = GetGpcDocumentTaskDefinition.builder()
-            .toAsid("to-asid-value")
-            .fromAsid("from-asid-value")
-            .build();
-
-        String transformedExtract = "<EhrExtract classCode=\"EXTRACT\" moodCode=\"EVN\"><id root=\"12345\"/></EhrExtract>";
+    public void When_MappingOutputMessageWrapperWithStringContent_Expect_ProperXmlOutput() {
         String outputMessage = outputMessageWrapperMapper.map(
             getGpcStructuredTaskDefinition,
-            transformedExtract);
-
-        CharSequence expectedOutputMessage = ResourceTestFileUtils.getFileContent("/ehr/mapper/expected-output-message-wrapper.xml");
+            TRANSFORMED_EXTRACT);
 
         assertThat(outputMessage).isEqualToIgnoringWhitespace(expectedOutputMessage);
     }
