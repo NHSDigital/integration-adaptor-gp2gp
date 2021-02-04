e1e0007052d0539ebbd32470b9abff73a469f342
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-02-04 14:08:53 +0000

    NIAD-844: Send Translated EHR Extract Core Message (#64)
    * Created outbound object for payload only, implemeneted mappers to convert fhir to hly and add to storage, refactored test accordingly.
    
    * Code review improvements
    
    * delete stub message

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/SendEhrExtractCoreTaskExecutor.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/SendEhrExtractCoreTaskExecutor.java
@@ -1,13 +1,22 @@
 package uk.nhs.adaptors.gp2gp.ehr;
 
+import static uk.nhs.adaptors.gp2gp.gpc.GpcFileNameConstants.GPC_STRUCTURED_FILE_EXTENSION;
+
 import java.time.Instant;
-import lombok.extern.slf4j.Slf4j;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import uk.nhs.adaptors.gp2gp.common.storage.StorageConnectorService;
 import uk.nhs.adaptors.gp2gp.common.task.TaskExecutor;
 import uk.nhs.adaptors.gp2gp.mhs.MhsClient;
 import uk.nhs.adaptors.gp2gp.mhs.MhsRequestBuilder;
+import uk.nhs.adaptors.gp2gp.mhs.model.OutboundMessageWithPayload;
 
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -16,6 +25,8 @@ public class SendEhrExtractCoreTaskExecutor implements TaskExecutor<SendEhrExtra
     private final MhsClient mhsClient;
     private final MhsRequestBuilder mhsRequestBuilder;
     private final EhrExtractStatusService ehrExtractStatusService;
+    private final StorageConnectorService storageConnectorService;
+    private final ObjectMapper objectMapper;
 
     @Override
     public Class<SendEhrExtractCoreTaskDefinition> getTaskType() {
@@ -23,10 +34,18 @@ public class SendEhrExtractCoreTaskExecutor implements TaskExecutor<SendEhrExtra
     }
 
     @Override
+    @SneakyThrows
     public void execute(SendEhrExtractCoreTaskDefinition sendEhrExtractCoreTaskDefinition) {
         LOGGER.info("SendEhrExtractCore task was created, Sending EHR extract to Spine");
 
-        var request = mhsRequestBuilder.buildSendEhrExtractCoreRequest();
+        var storageDataWrapper = storageConnectorService.downloadFile(
+            sendEhrExtractCoreTaskDefinition.getConversationId() + GPC_STRUCTURED_FILE_EXTENSION);
+        var extractCoreMessage = storageDataWrapper.getHl7Response();
+
+        var outboundMessage = OutboundMessageWithPayload.builder().payload(extractCoreMessage).build();
+        var stringRequestBody = objectMapper.writeValueAsString(outboundMessage);
+
+        var request = mhsRequestBuilder.buildSendEhrExtractCoreRequest(stringRequestBody);
         Instant requestSentAt = Instant.now();
         mhsClient.sendEhrExtractCore(request, sendEhrExtractCoreTaskDefinition);
 
