9b5e00a433f779b77425853a63cae5226850620a
Matt Dooner <mattd@kainos.com>
2021-02-04 19:56:46 +0000

    Added docker image scan

--- release/release.sh
+++ release/release.sh
@@ -9,6 +9,8 @@ docker-compose build gp2gp
 
 docker tag local/gp2gp:latest nhsdev/nia-gp2gp-adaptor:${RELEASE_VERSION}
 
+docker scan --severity high --file ../docker/service/Dockerfile --exclude-base nhsdev/nia-gp2gp-adaptor:${RELEASE_VERSION}
+
 if [ "$1" == "-y" ];
 then
   echo "Tagging and pushing Docker image and git tag"
@@ -16,4 +18,3 @@ then
   git tag -a ${RELEASE_VERSION} -m "Release ${RELEASE_VERSION}"
   git push origin ${RELEASE_VERSION}
 fi
-
