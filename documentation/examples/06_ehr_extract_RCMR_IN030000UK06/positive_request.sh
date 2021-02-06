c34c9c637d82cb6b7b45f6f63e17c0550da708e7
Matt Dooner <mattd@kainos.com>
2021-02-06 15:28:13 +0000

    replaced ids in 06

--- documentation/examples/06_ehr_extract_RCMR_IN030000UK06/positive_request.sh
+++ documentation/examples/06_ehr_extract_RCMR_IN030000UK06/positive_request.sh
@@ -4,19 +4,9 @@ source ../common.sh
 
 #Party Key X26-9199246
 
-EHR_EXTRACT=$(cat EhrExtract.xml)
+EHR_EXTRACT="$(envsubst < EhrExtract.xml)"
 PAYLOAD=$(json_escape "$EHR_EXTRACT")
 REQUEST_BODY="{\"payload\":$PAYLOAD}"
 INTERACTION_ID=RCMR_IN030000UK06
 
 mhs_request
-
-#curl -i -k -v -X POST \
-#-H "Content-Type: application/json" \
-#-H "Interaction-Id: RCMR_IN030000UK06" \
-#-H "from-asid: $MY_ASID" \
-#-H "wait-for-response: false" \
-#-H "Correlation-Id: $(uuidgen)" \
-#-H "ods-code: $GP2GP_RESPONDER_ODS" \
-#-d "$REQUEST_BODY" \
-#"http://localhost:80"
