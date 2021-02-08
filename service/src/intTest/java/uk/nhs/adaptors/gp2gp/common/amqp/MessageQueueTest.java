af464a8ec8d2c8847109b5cc86d2d16e1ee0295c
mmackiewicz1 <m.mackiewicz@kainos.com>
2021-02-08 17:01:51 +0100

    NIAD-932: Implemented some code change

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/common/amqp/MessageQueueTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/common/amqp/MessageQueueTest.java
@@ -1,7 +1,18 @@
 package uk.nhs.adaptors.gp2gp.common.amqp;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+import javax.jms.Message;
+
 import lombok.SneakyThrows;
+import uk.nhs.adaptors.gp2gp.ehr.EhrExtractStatusRepository;
+import uk.nhs.adaptors.gp2gp.mhs.InboundMessage;
+import uk.nhs.adaptors.gp2gp.mhs.InboundMessageHandler;
+import uk.nhs.adaptors.gp2gp.testcontainers.ActiveMQExtension;
+import uk.nhs.adaptors.gp2gp.testcontainers.MongoDBExtension;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +20,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jms.core.JmsTemplate;
-import uk.nhs.adaptors.gp2gp.ehr.EhrExtractStatusRepository;
-import uk.nhs.adaptors.gp2gp.mhs.InboundMessage;
-import uk.nhs.adaptors.gp2gp.mhs.InboundMessageHandler;
-import uk.nhs.adaptors.gp2gp.testcontainers.ActiveMQExtension;
-import uk.nhs.adaptors.gp2gp.testcontainers.MongoDBExtension;
 
-import javax.jms.Message;
-
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.verify;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @ExtendWith({MongoDBExtension.class, ActiveMQExtension.class})
@@ -27,12 +29,12 @@ public class MessageQueueTest {
     private static final String SOAP_HEADER = "<soap:Header></soap:Header>";
     private static final long TIMEOUT = 5000L;
 
-    @Autowired
-    private JmsTemplate jmsTemplate;
     @Value("${gp2gp.amqp.inboundQueueName}")
     private String inboundQueueName;
     @MockBean // mock the message handler to prevent any forward processing by the application
     private InboundMessageHandler inboundMessageHandler;
+    @Autowired
+    private JmsTemplate jmsTemplate;
     @Autowired
     private ObjectMapper objectMapper;
     @Autowired
@@ -48,7 +50,7 @@ public class MessageQueueTest {
         jmsTemplate.send(inboundQueueName, session -> session.createTextMessage(sentMessageContent));
 
         verify(inboundMessageHandler, timeout(TIMEOUT)).handle(argThat(jmsMessage ->
-                hasSameContentAsSentMessage(jmsMessage, sentMessageContent)
+            hasSameContentAsSentMessage(jmsMessage, sentMessageContent)
         ));
     }
 
