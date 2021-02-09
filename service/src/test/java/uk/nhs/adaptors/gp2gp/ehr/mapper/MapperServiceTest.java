8e85557fd8e6423910ad0d396a97b1de500fd55e
msolecka <m.solecka@kainos.com>
2021-02-09 14:36:32 +0100

    NIAD-814: Change timezone in test

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/MapperServiceTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/MapperServiceTest.java
@@ -6,11 +6,14 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.ZoneOffset;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Observation;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,11 +38,17 @@ public class MapperServiceTest {
 
     @BeforeEach
     public void setUp() throws ParseException {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
         when(randomIdGeneratorService.createNewId()).thenReturn("111122223333", "3334445555");
         mapperService = new MapperService(randomIdGeneratorService, new NarrativeStatementMapper());
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         date = simpleDateFormat.parse("2010-01-13 15:29:50.3+00:00");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        TimeZone.setDefault(null);
     }
 
     @Test
