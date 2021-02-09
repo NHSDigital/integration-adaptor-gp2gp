08cddf1e705091f46ad4891ffb7d408e6a2b54c1
msolecka <m.solecka@kainos.com>
2021-02-09 14:51:12 +0100

    NIAD-814: Remove executing mapping service form EhrExtractMapper because it is not the part of the story

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/EhrExtractMapper.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/EhrExtractMapper.java
@@ -34,7 +34,6 @@ public class EhrExtractMapper {
     private final FhirParseService fhirParseService;
     private final RandomIdGeneratorService randomIdGeneratorService;
     private final TimestampService timestampService;
-    private final MapperService mapperService;
 
     public EhrExtractTemplateParameters mapJsonToEhrFhirExtractParams(GetGpcStructuredTaskDefinition getGpcStructuredTaskDefinition,
             String json) {
@@ -62,8 +61,6 @@ public class EhrExtractMapper {
 
         extractPatientFromBundle(bundle)
             .orElseThrow(() -> new FhirValidationException("Missing patient resource in Fhir Bundle."));
-
-        mapperService.mapToHl7(bundle);
 
         return ehrExtractTemplateParameters;
     }
