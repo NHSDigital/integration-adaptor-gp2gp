bb76c7cf6d581a9d7ef5b79d983146ff55992578
z-uddin <74315663+z-uddin@users.noreply.github.com>
2021-02-11 08:54:42 +0000

    NIAD-920 Translating Uncategorised Observation to Observation Statement (#78)
    * Created a ObservationStatement mapper, which consumes FHIR Observation object and uses this to fill an Observation Statement XML template 
    * Unit tests parse a JSON file to a FHIR Observation object and then compares output from running map method to expected XML
    * Created a DateFormat util to convert dates to yyyyMMddHHmmss
    * Refactored unit tests in NarrativeStatementMapperTest to use parameterized test and argument stream for resource files

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/NarrativeStatementMapper.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/NarrativeStatementMapper.java
@@ -1,13 +1,9 @@
 package uk.nhs.adaptors.gp2gp.ehr.mapper;
 
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.util.Date;
-
 import lombok.RequiredArgsConstructor;
 import uk.nhs.adaptors.gp2gp.ehr.exception.EhrMapperException;
 import uk.nhs.adaptors.gp2gp.ehr.utils.TemplateUtils;
+import uk.nhs.adaptors.gp2gp.ehr.utils.DateFormatUtil;
 
 import org.hl7.fhir.dstu3.model.ResourceType;
 import org.hl7.fhir.dstu3.model.Observation;
@@ -22,11 +18,7 @@ public class NarrativeStatementMapper {
 
     private final MessageContext messageContext;
 
-    private static final String UK_ZONE_ID = "Europe/London";
     private static final Mustache NARRATIVE_STATEMENT_TEMPLATE = TemplateUtils.loadTemplate("ehr_narrative_statement_template.mustache");
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
-        .appendPattern("yyyyMMddHHmmss")
-        .toFormatter();
 
     public String mapObservationToNarrativeStatement(Observation observation, boolean isNested) {
         var narrativeStatementTemplateParameters = NarrativeStatementTemplateParameters.builder()
@@ -41,20 +33,13 @@ public class NarrativeStatementMapper {
 
     private String getAvailabilityTime(Observation observation) {
         if (observation.hasEffectiveDateTimeType() && observation.getEffectiveDateTimeType().hasValue()) {
-            return formatDate(observation.getEffectiveDateTimeType().getValue());
+            return DateFormatUtil.formatDate(observation.getEffectiveDateTimeType().getValue());
         } else if (observation.hasEffectivePeriod()) {
-            return formatDate(observation.getEffectivePeriod().getStart());
+            return DateFormatUtil.formatDate(observation.getEffectivePeriod().getStart());
         } else if (observation.hasIssued()) {
-            return formatDate(observation.getIssued());
+            return DateFormatUtil.formatDate((observation.getIssued()));
         } else {
             throw new EhrMapperException("Could not map effective date");
         }
-    }
-
-    private String formatDate(Date date) {
-        return DATE_TIME_FORMATTER.format(
-            date.toInstant()
-                .atZone(ZoneId.of(UK_ZONE_ID))
-                .toLocalDateTime());
     }
 }
---
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/ObservationStatementMapper.java
@@ -0,0 +1,47 @@
+package uk.nhs.adaptors.gp2gp.ehr.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.hl7.fhir.dstu3.model.Observation;
+
+import com.github.mustachejava.Mustache;
+import org.hl7.fhir.dstu3.model.ResourceType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.nhs.adaptors.gp2gp.ehr.utils.TemplateUtils;
+import uk.nhs.adaptors.gp2gp.ehr.utils.DateFormatUtil;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Component
+public class ObservationStatementMapper {
+
+    private static final Mustache OBSERVATION_STATEMENT_EFFECTIVE_TIME_TEMPLATE =
+        TemplateUtils.loadTemplate("ehr_observation_statement_effective_time_template.mustache");
+    private static final String EFFECTIVE_TIME_XML = "<effectiveTime><center value=\"%s\"/></effectiveTime>";
+    private static final String EFFECTIVE_PERIOD_XML = "<effectiveTime><low value=\"%s\"/><high value=\"%s\"/></effectiveTime>";
+    private static final String EFFECTIVE_TIME_UNK_XML =  "<effectiveTime><center value=\"UNK\"/></effectiveTime>";
+
+    private final MessageContext messageContext;
+
+    public String mapObservationToObservationStatement(Observation observation, boolean isNested) {
+        var observationStatementTemplateParameters = ObservationStatementTemplateParameters.builder()
+            .observationStatementId(messageContext.getIdMapper().getOrNew(ResourceType.Observation, observation.getId()))
+            .comment(observation.getComment())
+            .issued(DateFormatUtil.formatDate(observation.getIssued()))
+            .isNested(isNested)
+            .effectiveTime(effectiveTime(observation))
+            .build();
+
+        return TemplateUtils.fillTemplate(OBSERVATION_STATEMENT_EFFECTIVE_TIME_TEMPLATE, observationStatementTemplateParameters);
+    }
+
+    private String effectiveTime(Observation observation) {
+        if (observation.hasEffectiveDateTimeType() && observation.getEffectiveDateTimeType().hasValue()) {
+            return String.format(EFFECTIVE_TIME_XML, DateFormatUtil.formatDate(observation.getEffectiveDateTimeType().getValue()));
+        } else if (observation.hasEffectivePeriod()) {
+            return String.format(EFFECTIVE_PERIOD_XML, DateFormatUtil.formatDate(observation.getEffectivePeriod().getStart()),
+                DateFormatUtil.formatDate(observation.getEffectivePeriod().getEnd()));
+        } else {
+            return EFFECTIVE_TIME_UNK_XML;
+        }
+    }
+}
