5313e13dcfc99088aa762f95b25f335152b130a8
msolecka <m.solecka@kainos.com>
2021-02-11 15:12:08 +0100

    NIAD-917: Add ProcedureReport to PlanStatement mapping

---
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/DiaryPlanStatementMapper.java
@@ -0,0 +1,144 @@
+package uk.nhs.adaptors.gp2gp.ehr.mapper;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.dstu3.model.Annotation;
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.Period;
+import org.hl7.fhir.dstu3.model.ProcedureRequest;
+import org.hl7.fhir.dstu3.model.Reference;
+import org.hl7.fhir.dstu3.model.ResourceType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.mustachejava.Mustache;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import uk.nhs.adaptors.gp2gp.ehr.utils.DateFormatUtil;
+import uk.nhs.adaptors.gp2gp.ehr.utils.TemplateUtils;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class DiaryPlanStatementMapper {
+
+    private static final Mustache PLAN_STATEMENT_TEMPLATE = TemplateUtils.loadTemplate("ehr_plan_statement_template.mustache");
+    private static final String COMMA = ", ";
+    private static final String EMPTY_DATE = "nullFlavor=\"UNK\"";
+    private static final String FULL_DATE = "value=\"%s\"";
+
+    private final MessageContext messageContext;
+
+    public String mapDiaryProcedureRequestToPlanStatement(ProcedureRequest procedureRequest, Boolean isNested) {
+        if (procedureRequest.getIntent() != ProcedureRequest.ProcedureRequestIntent.PLAN) {
+            return StringUtils.EMPTY;
+        }
+
+        PlanStatementMapperParameters.PlanStatementMapperParametersBuilder builder = PlanStatementMapperParameters.builder()
+            .isNested(isNested)
+            .id(messageContext.getIdMapper().getOrNew(ResourceType.ProcedureRequest, procedureRequest.getId()));
+
+        buildAuthoredOn(procedureRequest).map(builder::authoredOn);
+        buildEffectiveTime(procedureRequest).map(builder::effectiveTime);
+        buildText(procedureRequest).map(builder::text);
+
+        return TemplateUtils.fillTemplate(PLAN_STATEMENT_TEMPLATE, builder.build());
+    }
+
+    private Optional<String> buildEffectiveTime(ProcedureRequest procedureRequest) {
+        Date date = null;
+        if (procedureRequest.hasOccurrenceDateTimeType()) {
+            date = procedureRequest.getOccurrenceDateTimeType().getValue();
+        } else if (procedureRequest.hasOccurrencePeriod()) {
+            Period occurrencePeriod = procedureRequest.getOccurrencePeriod();
+            date = occurrencePeriod.hasEnd() ? occurrencePeriod.getEnd() : occurrencePeriod.getStart();
+        }
+
+        return Optional.of(formatEffectiveDate(date));
+    }
+
+    private String formatEffectiveDate(Date date) {
+        return date != null ? String.format(FULL_DATE, DateFormatUtil.formatDate(date)) : EMPTY_DATE;
+    }
+
+    private Optional<String> buildAuthoredOn(ProcedureRequest procedureRequest) {
+        if (procedureRequest.hasAuthoredOn()) {
+            return Optional.of(DateFormatUtil.formatDate(procedureRequest.getAuthoredOn()));
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<String> buildText(ProcedureRequest procedureRequest) {
+        return Stream.of(
+            getRequester(procedureRequest),
+            getReasonCode(procedureRequest),
+            getNotes(procedureRequest)
+        )
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .reduce(joining());
+    }
+
+    private Optional<String> getReasonCode(ProcedureRequest procedureRequest) {
+        return procedureRequest.getReasonCode()
+            .stream()
+            .map(reasonCode -> {
+                if (StringUtils.isNoneBlank(reasonCode.getText())) {
+                    return reasonCode.getText();
+                }
+                return getDisplay(reasonCode);
+            })
+            .filter(StringUtils::isNoneBlank)
+            .reduce(joining());
+    }
+
+    private String getDisplay(org.hl7.fhir.dstu3.model.CodeableConcept reasonCode) {
+        return reasonCode.getCoding()
+            .stream()
+            .map(Coding::getDisplay)
+            .reduce(joining()).orElse(StringUtils.EMPTY);
+    }
+
+    private Optional<String> getNotes(ProcedureRequest procedureRequest) {
+        return procedureRequest.getNote()
+            .stream()
+            .map(Annotation::getText)
+            .reduce(joining());
+    }
+
+    private BinaryOperator<String> joining() {
+        return (value1, value2) -> value1 + COMMA + value2;
+    }
+
+    private Optional<String> getRequester(ProcedureRequest procedureRequest) {
+        Reference agent = procedureRequest.getRequester().getAgent();
+
+        if (agent.hasReference()) {
+            String reference = agent.getReference();
+            if (reference.startsWith(ResourceType.Organization.name())
+                || reference.startsWith(ResourceType.Device.name())) {
+                return Optional.of(agent.getDisplay());
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class PlanStatementMapperParameters {
+        private Boolean isNested;
+        private String id;
+        private String text;
+        private String authoredOn;
+        private String effectiveTime;
+    }
+}
--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/NarrativeStatementMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/NarrativeStatementMapperTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.time.ZoneOffset;
-import java.util.TimeZone;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,8 +17,6 @@ import uk.nhs.adaptors.gp2gp.utils.ResourceTestFileUtils;
 
 import org.hl7.fhir.dstu3.model.Observation;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,7 +24,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class NarrativeStatementMapperTest {
+public class NarrativeStatementMapperTest extends MapperTest {
     private static final String TEST_ID = "394559384658936";
     private static final String TEST_FILE_DIRECTORY = "/ehr/mapper/observation/";
     private static final String INPUT_JSON_WITH_EFFECTIVE_DATE_TIME = TEST_FILE_DIRECTORY + "example-observation-resource-1.json";
@@ -48,11 +44,6 @@ public class NarrativeStatementMapperTest {
     private NarrativeStatementMapper narrativeStatementMapper;
     private MessageContext messageContext;
 
-    @BeforeAll
-    public static void initialize() {
-        TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
-    }
-
     @BeforeEach
     public void setUp() {
         when(randomIdGeneratorService.createNewId()).thenReturn(TEST_ID);
@@ -63,11 +54,6 @@ public class NarrativeStatementMapperTest {
     @AfterEach
     public void tearDown() {
         messageContext.resetMessageContext();
-    }
-
-    @AfterAll
-    public static void deinitialize() {
-        TimeZone.setDefault(null);
     }
 
     @ParameterizedTest
