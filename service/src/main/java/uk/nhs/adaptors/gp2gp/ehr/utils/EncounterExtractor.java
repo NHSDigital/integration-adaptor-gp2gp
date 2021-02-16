92dffa45eab78d81652d2d0da58b54872f9988ab
mmackiewicz1 <m.mackiewicz@kainos.com>
2021-02-16 13:03:21 +0100

    NIAD-1040: Added extra protection for extracting coding

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/utils/EncounterExtractor.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/utils/EncounterExtractor.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.Encounter;
 import org.hl7.fhir.dstu3.model.ListResource;
 import org.hl7.fhir.dstu3.model.Reference;
@@ -42,10 +43,17 @@ public final class EncounterExtractor {
     }
 
     private static boolean isConsultationList(ListResource listResource) {
-        return listResource.getCode()
-            .getCoding()
-            .stream()
-            .anyMatch(coding -> coding.hasCode() && coding.getCode().equals(CONSULTATION_LIST_CODE));
+        if (listResource.hasCode()) {
+            CodeableConcept listCode = listResource.getCode();
+
+            if (listCode.hasCoding()) {
+                return listCode.getCoding()
+                    .stream()
+                    .anyMatch(coding -> coding.hasCode() && coding.getCode().equals(CONSULTATION_LIST_CODE))
+            }
+            return false;
+        }
+        return false;
     }
 
     private static List<String> extractEncounterReferencesFromListResource(List<ListResource.ListEntryComponent> entries) {
