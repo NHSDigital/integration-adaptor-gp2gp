ccb230cab3536a5111aaa6e664fd32b7362eae4f
Matt Dooner <mattd@kainos.com>
2021-02-17 09:34:05 +0000

    empty string

--- mock-mhs-adaptor/src/main/java/uk/nhs/adaptors/mockmhsservice/controller/MhsMockController.java
+++ mock-mhs-adaptor/src/main/java/uk/nhs/adaptors/mockmhsservice/controller/MhsMockController.java
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -39,9 +40,9 @@ public class MhsMockController {
             @RequestBody(required=false) String mockMhsMessage) {
 
         try {
-            String interactionId = Optional.ofNullable(headers.get("interaction-id")).orElse("");
-            String correlationId = Optional.ofNullable(headers.get("correlation-id")).orElse("");
-            String waitForResponse = Optional.ofNullable(headers.get("wait-for-response")).orElse("");
+            String interactionId = Optional.ofNullable(headers.get("interaction-id")).orElse(StringUtils.EMPTY);
+            String correlationId = Optional.ofNullable(headers.get("correlation-id")).orElse(StringUtils.EMPTY);
+            String waitForResponse = Optional.ofNullable(headers.get("wait-for-response")).orElse(StringUtils.EMPTY);
             return mockMhsService.handleRequest(interactionId, correlationId, waitForResponse, mockMhsMessage);
         } catch (Exception e) {
             LOGGER.error("Error could not process mock request", e);
