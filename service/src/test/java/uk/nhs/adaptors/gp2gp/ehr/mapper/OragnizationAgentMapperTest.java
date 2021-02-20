0f621ba88f0fef595dacbbb7b09aba5a0c1d1e5a
ElliotB-96 <elliotb@kainos.com>
2021-02-20 14:33:47 +0000

    Reverted previous change, changed mock to be lenient at class level

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/OragnizationAgentMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/OragnizationAgentMapperTest.java
@@ -1,7 +1,7 @@
 package uk.nhs.adaptors.gp2gp.ehr.mapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.stream.Stream;
@@ -14,12 +14,15 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import uk.nhs.adaptors.gp2gp.common.service.FhirParseService;
 import uk.nhs.adaptors.gp2gp.common.service.RandomIdGeneratorService;
 import uk.nhs.adaptors.gp2gp.utils.ResourceTestFileUtils;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class OragnizationAgentMapperTest {
 
     private static final String TEST_ID = "5E496953-065B-41F2-9577-BE8F2FBD0757";
@@ -53,7 +56,7 @@ public class OragnizationAgentMapperTest {
 
     @BeforeEach
     public void setUp() {
-        lenient().when(randomIdGeneratorService.createNewId()).thenReturn(TEST_ID);
+        when(randomIdGeneratorService.createNewId()).thenReturn(TEST_ID);
         MessageContext messageContext = new MessageContext(randomIdGeneratorService);
         organizationToAgentMapper = new OrganizationToAgentMapper(messageContext);
     }
