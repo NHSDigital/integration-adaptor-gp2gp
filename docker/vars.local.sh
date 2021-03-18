971c114c4d3e6a986587a527dd9914bec67d09c0
mmackiewicz1 <m.mackiewicz@kainos.com>
2021-03-18 20:36:56 +0100

    NIAD-1153: Implemented GPC Consumer onto GP2GP docker

--- docker/vars.local.sh
+++ docker/vars.local.sh
@@ -6,3 +6,7 @@ export GP2GP_MONGO_URI="mongodb://mongodb:27017"
 export GP2GP_MONGO_DATABASE_NAME="gp2gp"
 export GP2GP_MHS_OUTBOUND_URL="http://mock-mhs-adaptor:8081/mock-mhs-endpoint"
 export GP2GP_GPC_GET_URL="http://wiremock:8080/GP0001/STU3/1/gpconnect"
+
+export GPC_CONSUMER_URL="http://localhost:8220"
+export GPC_CONSUMER_GPC_GET_URL="http://localhost:8080"
+export GPC_CONSUMER_SDS_URL="http://wiremock:8080/"
