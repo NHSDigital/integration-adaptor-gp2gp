800b6b5c185f6bc8a42eef93b3974793b5e5989f
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-03-23 13:37:28 +0000

    replace resourceutils with getresourceasstring

--- mock-mhs-adaptor/src/main/java/uk/nhs/adaptors/mockmhsservice/common/ResourceReader.java
+++ mock-mhs-adaptor/src/main/java/uk/nhs/adaptors/mockmhsservice/common/ResourceReader.java
@@ -1,13 +1,19 @@
 package uk.nhs.adaptors.mockmhsservice.common;
 
-import java.nio.file.Files;
-import org.springframework.util.ResourceUtils;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.InputStream;
+
+import io.micrometer.core.instrument.util.IOUtils;
 import lombok.SneakyThrows;
 
 public class ResourceReader {
     @SneakyThrows
     public static String readAsString(String resourceName) {
-        var file = ResourceUtils.getFile(String.format("classpath:%s", resourceName));
-        return new String(Files.readAllBytes(file.toPath()));
+        try (InputStream inputStream = ResourceReader.class.getResourceAsStream(String.format("/%s",resourceName))) {
+            var resourceAsString = IOUtils.toString(inputStream, UTF_8);
+            inputStream.close();
+            return resourceAsString;
+        }
     }
 }
