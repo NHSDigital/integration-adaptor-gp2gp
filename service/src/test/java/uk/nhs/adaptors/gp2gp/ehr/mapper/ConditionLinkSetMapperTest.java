eecbaddb94b8409d547e6e62a8edd9106e3c8b91
Zakariyya Uddin <z.uddin@kainos.com>
2021-04-13 15:55:17 +0100

    Fixing EncounterComponents mapper test and adding test for non existent reference for Condition relatedMedicalContent

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/ConditionLinkSetMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/ConditionLinkSetMapperTest.java
@@ -1,7 +1,7 @@
 package uk.nhs.adaptors.gp2gp.ehr.mapper;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 
 import uk.nhs.adaptors.gp2gp.common.service.FhirParseService;
 import uk.nhs.adaptors.gp2gp.common.service.RandomIdGeneratorService;
+import uk.nhs.adaptors.gp2gp.ehr.exception.EhrMapperException;
 import uk.nhs.adaptors.gp2gp.utils.CodeableConceptMapperMockUtil;
 import uk.nhs.adaptors.gp2gp.utils.ResourceTestFileUtils;
 
@@ -18,6 +19,7 @@ import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.Condition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -47,6 +49,7 @@ public class ConditionLinkSetMapperTest {
     private static final String INPUT_JSON_DATES_PRESENT = CONDITION_FILE_LOCATIONS + "condition_dates_present.json";
     private static final String INPUT_JSON_DATES_NOT_PRESENT = CONDITION_FILE_LOCATIONS + "condition_dates_not_present.json";
     private static final String INPUT_JSON_RELATED_LIST_REFERENCE = CONDITION_FILE_LOCATIONS + "condition_related_list_reference.json";
+    private static final String INPUT_JSON_RELATED_NON_EXISTENT = CONDITION_FILE_LOCATIONS + "condition_related_non_existent.json";
 
     private static final String EXPECTED_OUTPUT_LINKSET = CONDITION_FILE_LOCATIONS + "expected_output_linkset_";
     private static final String OUTPUT_XML_WITH_IS_NESTED = EXPECTED_OUTPUT_LINKSET + "1.xml";
@@ -64,11 +67,12 @@ public class ConditionLinkSetMapperTest {
     private static final String OUTPUT_XML_WITH_DATES_PRESENT = EXPECTED_OUTPUT_LINKSET + "13.xml";
     private static final String OUTPUT_XML_WITH_DATES_NOT_PRESENT = EXPECTED_OUTPUT_LINKSET + "14.xml";
 
-    private MessageContext messageContext;
     @Mock
     private RandomIdGeneratorService randomIdGeneratorService;
     @Mock
     private CodeableConceptCdMapper codeableConceptCdMapper;
+
+    private MessageContext messageContext;
     private ConditionLinkSetMapper conditionLinkSetMapper;
     private FhirParseService fhirParseService;
     private Bundle bundle;
@@ -76,8 +80,6 @@ public class ConditionLinkSetMapperTest {
     @BeforeEach
     public void setUp() throws IOException {
         fhirParseService = new FhirParseService();
-        when(codeableConceptCdMapper.mapCodeableConceptToCd(any(CodeableConcept.class)))
-            .thenReturn(CodeableConceptMapperMockUtil.NULL_FLAVOR_CODE);
 
         var bundleInput = ResourceTestFileUtils.getFileContent(INPUT_JSON_BUNDLE);
         bundle = new FhirParseService().parseResource(bundleInput, Bundle.class);
@@ -86,7 +88,6 @@ public class ConditionLinkSetMapperTest {
         messageContext.initialize(bundle);
 
         conditionLinkSetMapper = new ConditionLinkSetMapper(messageContext, randomIdGeneratorService, codeableConceptCdMapper);
-        when(randomIdGeneratorService.createNewId()).thenReturn(GENERATED_ID);
     }
 
     @AfterEach
@@ -101,6 +102,10 @@ public class ConditionLinkSetMapperTest {
         var jsonInput = ResourceTestFileUtils.getFileContent(conditionJson);
         var expectedOutput = ResourceTestFileUtils.getFileContent(outputXml);
         Condition condition = fhirParseService.parseResource(jsonInput, Condition.class);
+
+        when(codeableConceptCdMapper.mapCodeableConceptToCd(any(CodeableConcept.class)))
+            .thenReturn(CodeableConceptMapperMockUtil.NULL_FLAVOR_CODE);
+        when(randomIdGeneratorService.createNewId()).thenReturn(GENERATED_ID);
 
         String outputMessage = conditionLinkSetMapper.mapConditionToLinkSet(condition, isNested);
         assertThat(outputMessage).isEqualTo(expectedOutput);
@@ -124,5 +129,15 @@ public class ConditionLinkSetMapperTest {
             Arguments.of(INPUT_JSON_NO_RELATED, OUTPUT_XML_WITH_NO_RELATED, false),
             Arguments.of(INPUT_JSON_RELATED_LIST_REFERENCE, OUTPUT_XML_WITH_NO_RELATED, false)
         );
+    }
+
+    @Test
+    public void When_MappingParsedObservationJsonWithNoAgentAlreadyMapped_Expect_MapperException() throws IOException {
+        var jsonInput = ResourceTestFileUtils.getFileContent(INPUT_JSON_RELATED_NON_EXISTENT);
+        Condition parsedObservation = fhirParseService.parseResource(jsonInput, Condition.class);
+
+        assertThatThrownBy(() -> conditionLinkSetMapper.mapConditionToLinkSet(parsedObservation, false))
+            .isExactlyInstanceOf(EhrMapperException.class)
+            .hasMessage("Could not resolve Condition Related Medical Content reference");
     }
 }
