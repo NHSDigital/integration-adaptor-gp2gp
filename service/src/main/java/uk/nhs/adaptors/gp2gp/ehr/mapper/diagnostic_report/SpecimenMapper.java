07b482f7eddd123566a4fc795a2b2776a691c732
msolecka <m.solecka@kainos.com>
2021-04-27 15:58:12 +0200

    NIAD-910: Speciment mapper implementation of pertinent information and participant, tests

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/diagnostic_report/SpecimenMapper.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/diagnostic_report/SpecimenMapper.java
@@ -1,37 +1,31 @@
 package uk.nhs.adaptors.gp2gp.ehr.mapper.diagnostic_report;
 
 import com.github.mustachejava.Mustache;
-import com.google.common.collect.ImmutableList;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.apache.commons.lang3.StringUtils;
-import org.hl7.fhir.dstu3.model.Attachment;
-import org.hl7.fhir.dstu3.model.Coding;
-import org.hl7.fhir.dstu3.model.DiagnosticReport;
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.DateTimeType;
+import org.hl7.fhir.dstu3.model.Duration;
 import org.hl7.fhir.dstu3.model.Observation;
+import org.hl7.fhir.dstu3.model.Reference;
 import org.hl7.fhir.dstu3.model.ResourceType;
-import org.hl7.fhir.dstu3.model.SampledData;
+import org.hl7.fhir.dstu3.model.SimpleQuantity;
 import org.hl7.fhir.dstu3.model.Specimen;
 import org.hl7.fhir.dstu3.model.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.nhs.adaptors.gp2gp.ehr.exception.EhrMapperException;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.CodeableConceptCdMapper;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.IdMapper;
+
 import uk.nhs.adaptors.gp2gp.ehr.mapper.MessageContext;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.ParticipantMapper;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.ParticipantType;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.StructuredObservationValueMapper;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.parameters.diagnostic_report.NarrativeStatementTemplateParameters;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.parameters.diagnostic_report.ObservationCompoundStatementTemplateParameters;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.parameters.diagnostic_report.ObservationStatementTemplateParameters;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.parameters.diagnostic_report.SpecimenCompoundStatementTemplateParameters;
+import uk.nhs.adaptors.gp2gp.ehr.utils.CodeableConceptMappingUtils;
 import uk.nhs.adaptors.gp2gp.ehr.utils.DateFormatUtil;
-import uk.nhs.adaptors.gp2gp.ehr.utils.StatementTimeMappingUtils;
 import uk.nhs.adaptors.gp2gp.ehr.utils.TemplateUtils;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -41,47 +35,27 @@ public class SpecimenMapper {
 
     private static final Mustache SPECIMEN_COMPOUND_STATEMENT_TEMPLATE =
         TemplateUtils.loadTemplate("specimen_compound_statement_template.mustache");
-    private static final String EFFECTIVE_TIME_TEMPLATE = "<effectiveTime><center value=\"%s\"></effectiveTime>";
+    private static final String FASTING_STATUS_URL = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect"
+        + "-FastingStatus-1";
+    private static final String EFFECTIVE_TIME_CENTER_TEMPLATE = "<center value=\"%s\"/>";
+    private static final String DEFAULT_TIME_VALUE = "<center nullFlavor=\"UNK\"/>";
 
     private final MessageContext messageContext;
     private final ObservationMapper observationMapper;
 
-    public String mapSpecimenToCompoundStatement(Specimen specimen, List<Observation> observations, DiagnosticReport diagnosticReport) {
-        List<Observation> observationsAssociatedWithSpecimen = observations.stream()
-            .filter(Observation::hasSpecimen)
-            .filter(observation -> observation.getSpecimen().getReference().equals(specimen.getId()))
-            .collect(Collectors.toList());
-
-        StringBuilder observationsBlock = new StringBuilder();
-        observationsAssociatedWithSpecimen.forEach(observationAssociatedWithSpecimen -> {
-            observationsBlock.append(
-                observationMapper.mapObservationToCompoundStatement(
-                    observationAssociatedWithSpecimen,
-                    observations
-                )
-            );
-        });
-
-        final IdMapper idMapper = messageContext.getIdMapper();
+    public String mapSpecimenToCompoundStatement(Specimen specimen, List<Observation> observations, String diagnosticReportIssuedDate) {
+        String mappedObservations = mapObservationsAssociatedWithSpecimen(specimen, observations);
 
         var specimenCompoundStatementTemplateParameters = SpecimenCompoundStatementTemplateParameters.builder()
-            .compoundStatementId(idMapper.getOrNew(ResourceType.Specimen, specimen.getId()))
-            .diagnosticReportIssuedDate(DateFormatUtil.toHl7Format(diagnosticReport.getIssuedElement()))
-            .accessionIdentifier(specimen.getAccessionIdentifier().getValue())
-            .effectiveTime(prepareEffectiveTimeElement(specimen))
-            .observations(observationsBlock.toString());
+            .compoundStatementId(messageContext.getIdMapper().getOrNew(ResourceType.Specimen, specimen.getId()))
+            .diagnosticReportIssuedDate(diagnosticReportIssuedDate)
+            .effectiveTime(prepareEffectiveTimeForSpecimen(specimen))
+            .observations(mappedObservations);
 
-        if (specimen.getType().hasCoding()) {
-            specimen.getType().getCoding().stream()
-                .findFirst()
-                .ifPresent(coding ->
-                    specimenCompoundStatementTemplateParameters.type(
-                        coding.getDisplay()
-                    )
-                );
-        } else if (specimen.getType().hasText())  {
-            specimenCompoundStatementTemplateParameters.type(specimen.getType().getText());
-        }
+        buildType(specimen).ifPresent(specimenCompoundStatementTemplateParameters::type);
+        buildAccessionIdentifier(specimen).ifPresent(specimenCompoundStatementTemplateParameters::accessionIdentifier);
+        buildPertinentInformation(specimen).ifPresent(specimenCompoundStatementTemplateParameters::pertinentInformation);
+        buildParticipant(specimen).ifPresent(specimenCompoundStatementTemplateParameters::participant);
 
         return TemplateUtils.fillTemplate(
             SPECIMEN_COMPOUND_STATEMENT_TEMPLATE,
@@ -89,21 +63,170 @@ public class SpecimenMapper {
         );
     }
 
-    private String prepareEffectiveTimeElement(Specimen specimen) {
-        if (specimen.getCollection().hasCollectedDateTimeType()) {
-            return String.format(
-                EFFECTIVE_TIME_TEMPLATE,
-                DateFormatUtil.toHl7Format(specimen.getCollection().getCollectedDateTimeType())
-            );
-        }
+    public static String prepareEffectiveTimeForSpecimen(Specimen specimen) {
+        Optional<DateTimeType> effectiveDate = getCollectionDateTime(specimen);
+
+        return effectiveDate.map(date -> String.format(EFFECTIVE_TIME_CENTER_TEMPLATE, DateFormatUtil.toHl7Format(date)))
+            .orElse(DEFAULT_TIME_VALUE);
+    }
+
+    private static Optional<DateTimeType> getCollectionDateTime(Specimen specimen) {
+        Optional<DateTimeType> effectiveDate = Optional.empty();
 
         if (specimen.hasReceivedTime()) {
-            return String.format(
-                EFFECTIVE_TIME_TEMPLATE,
-                DateFormatUtil.toHl7Format(specimen.getReceivedTimeElement())
-            );
+            effectiveDate = Optional.of(specimen.getReceivedTimeElement());
         }
 
-        return StringUtils.EMPTY;
+        if (specimen.hasCollection()) {
+            Specimen.SpecimenCollectionComponent collection = specimen.getCollection();
+            if (collection.hasCollectedDateTimeType()) {
+                effectiveDate = Optional.of(collection.getCollectedDateTimeType());
+            } else if (collection.hasCollectedPeriod()) {
+                effectiveDate = Optional.of(collection.getCollectedPeriod().getStartElement());
+            }
+        }
+
+        return effectiveDate;
+    }
+
+    private Optional<String> buildParticipant(Specimen specimen) {
+        if (specimen.hasCollection() && specimen.getCollection().hasCollector()) {
+            Reference collector = specimen.getCollection().getCollector();
+
+            return Optional.of(messageContext.getIdMapper().get(collector));
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<String> buildAccessionIdentifier(Specimen specimen) {
+        if (specimen.hasAccessionIdentifier()) {
+            return Optional.of(specimen.getAccessionIdentifier().getValue());
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<String> buildType(Specimen specimen) {
+        if (specimen.hasType()) {
+            return CodeableConceptMappingUtils.extractTextOrCoding(specimen.getType());
+        }
+
+        return Optional.empty();
+    }
+
+    private String mapObservationsAssociatedWithSpecimen(Specimen specimen, List<Observation> observations) {
+        List<Observation> observationsAssociatedWithSpecimen = observations.stream()
+            .filter(Observation::hasSpecimen)
+            .filter(observation -> observation.getSpecimen().getReference().equals(specimen.getId()))
+            .collect(Collectors.toList());
+
+        return observationsAssociatedWithSpecimen.stream().map(
+            observationAssociatedWithSpecimen -> observationMapper.mapObservationToCompoundStatement(
+                observationAssociatedWithSpecimen,
+                observations
+            ))
+            .collect(Collectors.joining());
+    }
+
+    private Optional<String> buildPertinentInformation(Specimen specimen) {
+        PertinentInformationBuilder pertinentInformationBuilder = new PertinentInformationBuilder();
+        if (specimen.hasCollection()) {
+            Specimen.SpecimenCollectionComponent collection = specimen.getCollection();
+
+            collection.getExtensionsByUrl(FASTING_STATUS_URL).stream().findFirst()
+                .ifPresent(extension -> {
+                    Type value = extension.getValue();
+                    if (value instanceof CodeableConcept) {
+                        pertinentInformationBuilder.fastingStatus((CodeableConcept) value);
+                    } else if (value instanceof Duration) {
+                        pertinentInformationBuilder.fastingDuration((Duration) value);
+                    }
+                });
+
+            if (collection.hasQuantity()) {
+                pertinentInformationBuilder.quantity(collection.getQuantity());
+            }
+
+            if (collection.hasBodySite()) {
+                pertinentInformationBuilder.collectionSite(collection.getBodySite());
+            }
+        }
+
+        specimen.getNote().forEach(note -> pertinentInformationBuilder.note(note.getText()));
+
+        return getCollectionDateTime(specimen)
+            .map(pertinentInformationBuilder::build)
+            .orElseGet(pertinentInformationBuilder::build);
+    }
+
+    private static class PertinentInformationBuilder {
+
+        private static final String FASTING_STATUS = "Fasting Status:";
+        private static final String FASTING_DURATION = "Fasting Duration:";
+        private static final String QUANTITY = "Quantity:";
+        private static final String COLLECTION_SITE = "Collection Site:";
+        private static final String COMMENT_PREFIX = "comment type - LAB SPECIMEN COMMENT(E271)";
+
+        private String pertinentInformation;
+
+        PertinentInformationBuilder() {
+            pertinentInformation = StringUtils.EMPTY;
+        }
+
+        public void fastingStatus(CodeableConcept fastingStatus) {
+            CodeableConceptMappingUtils.extractTextOrCoding(fastingStatus)
+                .map(fastingStatusValue -> newLine(withSpace(FASTING_STATUS, fastingStatusValue), pertinentInformation))
+                .ifPresent(text -> pertinentInformation = text);
+        }
+
+        public void fastingDuration(Duration fastingDuration) {
+            pertinentInformation = newLine(
+                withSpace(FASTING_DURATION, fastingDuration.getValue().toString(), fastingDuration.getUnit()),
+                pertinentInformation);
+        }
+
+        public void quantity(SimpleQuantity quantity) {
+            pertinentInformation = newLine(
+                withSpace(QUANTITY, quantity.getValue().toString(), quantity.getUnit()),
+                pertinentInformation);
+        }
+
+        public void collectionSite(CodeableConcept collectionSite) {
+            CodeableConceptMappingUtils.extractTextOrCoding(collectionSite)
+                .map(collectionSiteValue -> newLine(withSpace(COLLECTION_SITE, collectionSiteValue), pertinentInformation))
+                .ifPresent(text -> pertinentInformation = text);
+        }
+
+        public void note(String note) {
+            pertinentInformation = newLine(note, pertinentInformation);
+        }
+
+        public Optional<String> build(DateTimeType date) {
+            if (StringUtils.isNotBlank(pertinentInformation)) {
+                return Optional.of(newLine(withSpace(COMMENT_PREFIX, DateFormatUtil.toTextFormat(date)), pertinentInformation));
+            }
+
+            return Optional.empty();
+        }
+
+        public Optional<String> build() {
+            if (StringUtils.isNotBlank(pertinentInformation)) {
+                return Optional.of(newLine(COMMENT_PREFIX, pertinentInformation));
+            }
+
+            return Optional.empty();
+        }
+
+        private String newLine(String text, String pertinentInformation) {
+            return StringUtils.joinWith(
+                StringUtils.LF,
+                text,
+                pertinentInformation);
+        }
+
+        private String withSpace(Object... values) {
+            return StringUtils.joinWith(StringUtils.SPACE, values);
+        }
     }
 }
---
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/SpecimenMapperTest.java
@@ -0,0 +1,123 @@
+package uk.nhs.adaptors.gp2gp.ehr.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.hl7.fhir.dstu3.model.Observation;
+import org.hl7.fhir.dstu3.model.Reference;
+import org.hl7.fhir.dstu3.model.ResourceType;
+import org.hl7.fhir.dstu3.model.Specimen;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import uk.nhs.adaptors.gp2gp.common.service.FhirParseService;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnostic_report.ObservationMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnostic_report.SpecimenMapper;
+import uk.nhs.adaptors.gp2gp.utils.ResourceTestFileUtils;
+
+@ExtendWith(MockitoExtension.class)
+public class SpecimenMapperTest {
+
+    private static final String TEST_FILE_DIRECTORY = "/ehr/mapper/specimen/";
+    private static final String DIAGNOSTIC_REPORT_DATE = "2020-10-12";
+    private static final String INPUT_OBSERVATION_RELATED_TO_SPECIMEN = "input-observation-1.json";
+    private static final String INPUT_OBSERVATION_NOT_RELATED_TO_SPECIMEN = "input-observation-2.json";
+
+    private SpecimenMapper specimenMapper;
+
+    @Mock
+    private MessageContext messageContext;
+    @Mock
+    private IdMapper idMapper;
+    @Mock
+    private ObservationMapper observationMapper;
+    private List<Observation> observations;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        lenient().when(messageContext.getIdMapper()).thenReturn(idMapper);
+        lenient().when(idMapper.getOrNew(any(ResourceType.class), anyString())).thenAnswer(mockId());
+        lenient().when(idMapper.get(any(Reference.class))).thenAnswer(mockReference());
+
+        observations = Arrays.asList(
+            parseObservation(INPUT_OBSERVATION_RELATED_TO_SPECIMEN),
+            parseObservation(INPUT_OBSERVATION_NOT_RELATED_TO_SPECIMEN)
+        );
+
+        when(observationMapper.mapObservationToCompoundStatement(any(), any())).thenAnswer(mockObservationMapping());
+
+        specimenMapper = new SpecimenMapper(messageContext, observationMapper);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    public void When_MappingSpecimen_Expect_XmlMapped(String inputPath, String expectedPath) throws IOException {
+        var input = ResourceTestFileUtils.getFileContent(TEST_FILE_DIRECTORY + inputPath);
+        var expected = ResourceTestFileUtils.getFileContent(TEST_FILE_DIRECTORY + expectedPath);
+        var specimen = new FhirParseService().parseResource(input, Specimen.class);
+
+        String outputMessage = specimenMapper.mapSpecimenToCompoundStatement(specimen, observations, DIAGNOSTIC_REPORT_DATE);
+
+        assertThat(outputMessage).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+            Arguments.of("input-specimen.json", "expected-specimen.xml"),
+            Arguments.of("input-specimen-with-collection-period.json", "expected-specimen-with-collection-period.xml"),
+            Arguments.of("input-specimen-without-accession-identifier.json", "expected-specimen-without-accession-identifier.xml"),
+            Arguments.of("input-specimen-without-collection-time.json", "expected-specimen-without-collection-time.xml"),
+            Arguments.of("input-specimen-without-type.json", "expected-specimen-without-type.xml"),
+            Arguments.of("input-specimen-without-type-text.json", "expected-specimen-without-type-text.xml"),
+            Arguments.of("input-specimen-without-agent-person.json", "expected-specimen-without-agent-person.xml"),
+            Arguments.of("input-specimen-without-pertinent-information.json", "expected-specimen-without-pertinent-information.xml"),
+            Arguments.of("input-specimen-without-fasting-status.json", "expected-specimen-without-fasting-status.xml"),
+            Arguments.of("input-specimen-without-fasting-duration.json", "expected-specimen-without-fasting-duration.xml"),
+            Arguments.of("input-specimen-without-quantity.json", "expected-specimen-without-quantity.xml"),
+            Arguments.of("input-specimen-without-body-site.json", "expected-specimen-without-body-site.xml"),
+            Arguments.of("input-specimen-without-notes.json", "expected-specimen-without-notes.xml")
+        );
+    }
+
+    private Answer<String> mockId() {
+        return invocation -> {
+            ResourceType resourceType = invocation.getArgument(0);
+            String originalId = invocation.getArgument(1);
+            return String.format("ID-for-%s-%s", resourceType.name(), originalId);
+        };
+    }
+
+    private Answer<String> mockReference() {
+        return invocation -> {
+            Reference reference = invocation.getArgument(0);
+            return String.format("REFERENCE-to-%s", reference.getReference());
+        };
+    }
+
+    private Answer<String> mockObservationMapping() {
+        return invocation -> {
+            Observation observation = invocation.getArgument(0);
+            return String.format("<!-- Mapped Observation with id: %s -->", observation.getId());
+        };
+    }
+
+    private Observation parseObservation(String path) throws IOException {
+        String fileContent = ResourceTestFileUtils.getFileContent(TEST_FILE_DIRECTORY + path);
+
+        return new FhirParseService().parseResource(fileContent, Observation.class);
+    }
+}
