42a2540547ebcf5e12ba4f8fa06293da2747720e
n-r-b <nblanchette@companieshouse.gov.uk>
2021-04-28 15:07:45 +0100

    NIAD-1404: add diagnostic report mapper tests

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/DiagnosticReportMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/DiagnosticReportMapperTest.java
@@ -1,11 +1,14 @@
 package uk.nhs.adaptors.gp2gp.ehr.mapper;
 
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.DiagnosticReport;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
@@ -13,33 +16,55 @@ import org.mockito.quality.Strictness;
 import uk.nhs.adaptors.gp2gp.common.service.FhirParseService;
 import uk.nhs.adaptors.gp2gp.common.service.RandomIdGeneratorService;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnostic_report.DiagnosticReportMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnostic_report.ObservationMapper;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnostic_report.SpecimenMapper;
+import uk.nhs.adaptors.gp2gp.utils.CodeableConceptMapperMockUtil;
 import uk.nhs.adaptors.gp2gp.utils.ResourceTestFileUtils;
 
 import java.io.IOException;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class DiagnosticReportMapperTest {
-    private static final String DIAGNOSTIC_REPORT_FILE_LOCATION = "/ehr/mapper/diagnostic_report/";
-
-    private static final String INPUT_JSON_BUNDLE = DIAGNOSTIC_REPORT_FILE_LOCATION + "fhir_bundle.json";
-    private static final String INPUT_JSON_DIAGNOSTIC_REPORT = DIAGNOSTIC_REPORT_FILE_LOCATION + "diagnostic_report.json";
-//    private static final String OUTPUT_XML = DIAGNOSTIC_REPORT_FILE_LOCATION
-//        + "output.xml";
+    private static final String TEST_ID = "394559384658936";
+    private static final String TEST_FILE_DIRECTORY = "/ehr/mapper/diagnostic_report/";
+    private static final String INPUT_JSON_BUNDLE = TEST_FILE_DIRECTORY + "fhir-bundle.json";
+    private static final String INPUT_JSON_REQUIRED_DATA = TEST_FILE_DIRECTORY + "diagnostic-report-with-required-data.json";
+    private static final String INPUT_JSON_EMPTY_SPECIMENS = TEST_FILE_DIRECTORY + "diagnostic-report-with-empty-specimens.json";
+    private static final String INPUT_JSON_ONE_SPECIMEN = TEST_FILE_DIRECTORY + "diagnostic-report-with-one-specimen.json";
+    private static final String INPUT_JSON_MULTI_SPECIMENS = TEST_FILE_DIRECTORY + "diagnostic-report-with-multi-specimens.json";
+    private static final String OUTPUT_XML_REQUIRED_DATA = TEST_FILE_DIRECTORY + "diagnostic-report-with-required-data.xml";
+    private static final String OUTPUT_XML_ONE_SPECIMEN = TEST_FILE_DIRECTORY + "diagnostic-report-with-one-specimen.xml";
+    private static final String OUTPUT_XML_MULTI_SPECIMENS = TEST_FILE_DIRECTORY + "diagnostic-report-with-multi-specimens.xml";
 
     @Mock
     private RandomIdGeneratorService randomIdGeneratorService;
-
     @Mock
-    private SpecimenMapper specimenMapper;
+    private CodeableConceptCdMapper codeableConceptCdMapper;
 
+    private DiagnosticReportMapper mapper;
     private MessageContext messageContext;
-    private FhirParseService fhirParseService;
 
     @BeforeEach
     public void setUp() throws IOException {
-        fhirParseService = new FhirParseService();
+        when(randomIdGeneratorService.createNewId()).thenReturn(TEST_ID);
+        final String bundleInput = ResourceTestFileUtils.getFileContent(INPUT_JSON_BUNDLE);
+        final Bundle bundle = new FhirParseService().parseResource(bundleInput, Bundle.class);
+        messageContext = new MessageContext(randomIdGeneratorService);
+        messageContext.initialize(bundle);
+
+        when(codeableConceptCdMapper.mapCodeableConceptToCd(any(CodeableConcept.class)))
+            .thenReturn(CodeableConceptMapperMockUtil.NULL_FLAVOR_CODE);
+        final ObservationMapper observationMapper = new ObservationMapper(messageContext,
+            new StructuredObservationValueMapper(), codeableConceptCdMapper, new ParticipantMapper());
+        final SpecimenMapper specimenMapper = new SpecimenMapper(messageContext,
+            observationMapper);
+        mapper = new DiagnosticReportMapper(messageContext, specimenMapper);
     }
 
     @AfterEach
@@ -47,23 +72,22 @@ public class DiagnosticReportMapperTest {
         messageContext.resetMessageContext();
     }
 
-    @Test
-    public void When_MappingDiagnosticReportJson_Expect_CompoundStatementXmlOutput() throws IOException {
-        var jsonInput = ResourceTestFileUtils.getFileContent(INPUT_JSON_BUNDLE);
-        var jsonDiagnosticReportInput = ResourceTestFileUtils.getFileContent(INPUT_JSON_DIAGNOSTIC_REPORT);
-//        var expectedOutput = ResourceTestFileUtils.getFileContent(outputXml);
+    @ParameterizedTest
+    @MethodSource("resourceFileParams")
+    public void When_MappingDiagnosticReportJson_Expect_CompoundStatementXmlOutput(String inputJson, String outputXml) throws IOException {
+        final CharSequence expectedOutputMessage = ResourceTestFileUtils.getFileContent(outputXml);
+        final String jsonInput = ResourceTestFileUtils.getFileContent(inputJson);
+        final DiagnosticReport diagnosticReport = new FhirParseService().parseResource(jsonInput, DiagnosticReport.class);
 
-        Bundle bundle = fhirParseService.parseResource(jsonInput, Bundle.class);
+        final String outputMessage = mapper.mapDiagnosticReportToCompoundStatement(diagnosticReport);
 
-        messageContext = new MessageContext(randomIdGeneratorService);
-        messageContext.initialize(bundle);
+        assertThat(outputMessage).isEqualTo(expectedOutputMessage);
+    }
 
-        DiagnosticReport diagnosticReport = fhirParseService.parseResource(jsonDiagnosticReportInput, DiagnosticReport.class);
-
-        DiagnosticReportMapper diagnosticReportMapper = new DiagnosticReportMapper(messageContext, specimenMapper);
-
-        String outputMessage = diagnosticReportMapper.mapDiagnosticReportToCompoundStatement(diagnosticReport);
-
-//        assertThat(outputMessage).isEqualToIgnoringWhitespace(expectedOutput);
+    private static Stream<Arguments> resourceFileParams() {
+        return Stream.of(Arguments.of(INPUT_JSON_REQUIRED_DATA, OUTPUT_XML_REQUIRED_DATA),
+            Arguments.of(INPUT_JSON_EMPTY_SPECIMENS, OUTPUT_XML_REQUIRED_DATA),
+            Arguments.of(INPUT_JSON_ONE_SPECIMEN, OUTPUT_XML_ONE_SPECIMEN),
+            Arguments.of(INPUT_JSON_MULTI_SPECIMENS, OUTPUT_XML_MULTI_SPECIMENS));
     }
 }
