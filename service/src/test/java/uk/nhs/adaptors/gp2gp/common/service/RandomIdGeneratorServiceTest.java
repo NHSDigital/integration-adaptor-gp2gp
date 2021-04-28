5db670fe3b741501ce2c891e6a720be0e70cd2f6
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2021-04-28 11:52:32 +0200

    NIAD-1339 using full UUID regexp in tests

--- service/src/test/java/uk/nhs/adaptors/gp2gp/common/service/RandomIdGeneratorServiceTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/common/service/RandomIdGeneratorServiceTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 
 public class RandomIdGeneratorServiceTest {
 
+    private static final String UUID_UPPERCASE_REGEXP = "[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}";
+
     @Test
     public void When_GeneratingRandomId_Expect_GeneratedIdIsRandomUUID() {
         String id1 = new RandomIdGeneratorService().createNewId();
@@ -19,8 +21,8 @@ public class RandomIdGeneratorServiceTest {
             () -> assertThatCode(() -> UUID.fromString(id1)).doesNotThrowAnyException(),
             () -> assertThatCode(() -> UUID.fromString(id2)).doesNotThrowAnyException(),
             () -> assertThat(id1).isNotEqualTo(id2),
-            () -> assertThat(id1).matches("[A-Z0-9-]+"),
-            () -> assertThat(id2).matches("[A-Z0-9-]+")
+            () -> assertThat(id1).matches(UUID_UPPERCASE_REGEXP),
+            () -> assertThat(id2).matches(UUID_UPPERCASE_REGEXP)
         );
     }
 }
