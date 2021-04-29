fc541f120cd71ea6d2387045c77a21c1ce9c1a6b
dawidskainos <d.stelmaszek@kainos.com>
2021-04-29 13:01:55 +0200

    Niad 1339 uppercase UUIDs (#188)
    * NIAD-1339 generate uppercase UUIDs
    
    * NIAD-1339 using full UUID regexp in tests
    
    * NIAD-1339 uppercase UUIDs in UAT
    
    * UAT tests adjustment
    
    Co-authored-by: n-r-b <nblanchette@companieshouse.gov.uk>
    Co-authored-by: ElliotB-96 <elliotb@kainos.com>

--- service/src/test/java/uk/nhs/adaptors/gp2gp/common/service/RandomIdGeneratorServiceTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/common/service/RandomIdGeneratorServiceTest.java
@@ -2,6 +2,7 @@ package uk.nhs.adaptors.gp2gp.common.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.UUID;
 
@@ -9,15 +10,19 @@ import org.junit.jupiter.api.Test;
 
 public class RandomIdGeneratorServiceTest {
 
+    private static final String UUID_UPPERCASE_REGEXP = "[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}";
+
     @Test
     public void When_GeneratingRandomId_Expect_GeneratedIdIsRandomUUID() {
         String id1 = new RandomIdGeneratorService().createNewId();
         String id2 = new RandomIdGeneratorService().createNewId();
-        assertThatCode(() -> UUID.fromString(id1))
-            .doesNotThrowAnyException();
-        assertThatCode(() -> UUID.fromString(id2))
-            .doesNotThrowAnyException();
-        assertThat(id1).isNotEqualTo(id2);
-    }
 
+        assertAll(
+            () -> assertThatCode(() -> UUID.fromString(id1)).doesNotThrowAnyException(),
+            () -> assertThatCode(() -> UUID.fromString(id2)).doesNotThrowAnyException(),
+            () -> assertThat(id1).isNotEqualTo(id2),
+            () -> assertThat(id1).matches(UUID_UPPERCASE_REGEXP),
+            () -> assertThat(id2).matches(UUID_UPPERCASE_REGEXP)
+        );
+    }
 }
