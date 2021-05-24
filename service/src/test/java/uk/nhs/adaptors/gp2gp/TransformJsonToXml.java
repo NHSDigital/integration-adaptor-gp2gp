e4d49ea18a18e678f8bee10af97ca0cac1d1cd21
AlanSimpson1723 <56081521+AlanSimpson1723@users.noreply.github.com>
2021-05-24 15:01:01 +0100

    checkstyle fixes

--- service/src/test/java/uk/nhs/adaptors/gp2gp/TransformJsonToXml.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/TransformJsonToXml.java
@@ -11,13 +11,36 @@ import org.json.JSONObject;
 import uk.nhs.adaptors.gp2gp.common.service.FhirParseService;
 import uk.nhs.adaptors.gp2gp.common.service.RandomIdGeneratorService;
 import uk.nhs.adaptors.gp2gp.common.service.TimestampService;
-import uk.nhs.adaptors.gp2gp.ehr.mapper.*;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.AgentDirectoryMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.AllergyStructureMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.BloodPressureMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.CodeableConceptCdMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.ConditionLinkSetMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.DiaryPlanStatementMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.DocumentReferenceToNarrativeStatementMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.EhrExtractMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.EncounterComponentsMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.EncounterMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.ImmunizationObservationStatementMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.MedicationStatementMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.NonConsultationResourceMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.ObservationStatementMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.ObservationToNarrativeStatementMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.OrganizationToAgentMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.OutputMessageWrapperMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.ParticipantMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.
+        PertinentInformationObservationValueMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.
+        PractitionerAgentPersonMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.RequestStatementMapper;
+import uk.nhs.adaptors.gp2gp.ehr.mapper.StructuredObservationValueMapper;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnosticreport.DiagnosticReportMapper;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnosticreport.ObservationMapper;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.diagnosticreport.SpecimenMapper;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.parameters.EhrExtractTemplateParameters;
 import uk.nhs.adaptors.gp2gp.gpc.GetGpcStructuredTaskDefinition;
-
+import uk.nhs.adaptors.gp2gp.ehr.mapper.MessageContext;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -29,18 +52,20 @@ import java.util.List;
 
 public class TransformJsonToXml {
 
-    private static final String JSON_FILE_INPUT_PATH = Paths.get("src", "test", "resources", "JsonToXml", "Input").toFile().getAbsoluteFile().getAbsolutePath() + "/";
-    private static final String XML_OUTPUT_PATH = Paths.get("src", "test", "resources", "JsonToXml", "Output").toFile().getAbsoluteFile().getAbsolutePath() + "/";
+    private static final String JSON_FILE_INPUT_PATH =
+            Paths.get("src", "test", "resources", "JsonToXml", "Input").toFile().getAbsoluteFile().getAbsolutePath() + "/";
+    private static final String XML_OUTPUT_PATH =
+            Paths.get("src", "test", "resources", "JsonToXml", "Output").toFile().getAbsoluteFile().getAbsolutePath() + "/";
 
     public static void main(String[] args) throws Exception {
 
         var inputWrapper = getFiles();
 
         for (int i = 0; i < inputWrapper.getJsonFileInputs().size(); i++) {
-            var JsonString = inputWrapper.getJsonFileInputs().get(i);
-            String XmlResult = mapJsonToXml(JsonString);
+            var jsonString = inputWrapper.getJsonFileInputs().get(i);
+            String xmlResult = mapJsonToXml(jsonString);
             var fileName = inputWrapper.getJsonFileNames().get(i);
-            writeToFile(XmlResult, fileName);
+            writeToFile(xmlResult, fileName);
         }
     }
 
@@ -50,20 +75,20 @@ public class TransformJsonToXml {
         List<String> jsonStringInputs = new ArrayList<>();
         List<String> fileNames = new ArrayList<>();
 
-        for (File JsonFile : files) {
-            if (FilenameUtils.getExtension(JsonFile.getName()).equalsIgnoreCase("json")) {
+        for (File jsonFile : files) {
+            if (FilenameUtils.getExtension(jsonFile.getName()).equalsIgnoreCase("json")) {
                 try {
-                    String jsonAsString = readJsonFileAsString(JSON_FILE_INPUT_PATH + JsonFile.getName());
+                    String jsonAsString = readJsonFileAsString(JSON_FILE_INPUT_PATH + jsonFile.getName());
                     jsonStringInputs.add(jsonAsString);
-                    fileNames.add(JsonFile.getName());
+                    fileNames.add(jsonFile.getName());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
         return InputWrapper.builder()
-                .JsonFileInputs(jsonStringInputs)
-                .JsonFileNames(fileNames)
+                .jsonFileInputs(jsonStringInputs)
+                .jsonFileNames(fileNames)
                 .build();
     }
 
@@ -89,9 +114,9 @@ public class TransformJsonToXml {
         writer.close();
     }
 
-    private static String mapJsonToXml(String JsonAsStringInput) throws JSONException {
+    private static String mapJsonToXml(String jsonAsStringInput) throws JSONException {
 
-        final Bundle bundle = new FhirParseService().parseResource(JsonAsStringInput, Bundle.class);
+        final Bundle bundle = new FhirParseService().parseResource(jsonAsStringInput, Bundle.class);
 
         final RandomIdGeneratorService randomIdGeneratorService = new RandomIdGeneratorServiceStub();
 
@@ -102,7 +127,7 @@ public class TransformJsonToXml {
         GetGpcStructuredTaskDefinition getGpcStructuredTaskDefinition;
 
         getGpcStructuredTaskDefinition = GetGpcStructuredTaskDefinition.builder()
-                .nhsNumber(stringToJson(JsonAsStringInput))
+                .nhsNumber(stringToJson(jsonAsStringInput))
                 .conversationId("6910A49D-1F97-4AA0-9C69-197EE9464C76")
                 .requestId("17A3A644-A4EB-4C0A-A870-152D310FD1F8")
                 .fromOdsCode("GP2GPTEST")
@@ -177,8 +202,8 @@ public class TransformJsonToXml {
     @Builder
     @AllArgsConstructor
     public static class InputWrapper {
-        private List<String> JsonFileNames;
-        private List<String> JsonFileInputs;
+        private List<String> jsonFileNames;
+        private List<String> jsonFileInputs;
     }
 }
 
