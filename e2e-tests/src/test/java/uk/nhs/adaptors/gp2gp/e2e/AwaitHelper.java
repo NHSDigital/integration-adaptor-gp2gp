c98599106f15eaa8b20233a1f239ca128cd6c17f
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-06-01 14:28:44 +0100

    increase wait time to 30 seconds

--- e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/AwaitHelper.java
+++ e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/AwaitHelper.java
@@ -10,7 +10,7 @@ import java.util.function.Supplier;
 
 public class AwaitHelper {
 
-    protected static final int WAIT_FOR_IN_SECONDS = 20;
+    protected static final int WAIT_FOR_IN_SECONDS = 30;
     protected static final int POLL_INTERVAL_MS = 100;
     protected static final int POLL_DELAY_MS = 10;
 
