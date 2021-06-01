939803a769ee531e1fa648d6b13ab2da379ed055
ElliotB-96 <elliotb@kainos.com>
2021-06-01 12:15:55 +0100

    Extended wait time in helper function

--- e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/AwaitHelper.java
+++ e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/AwaitHelper.java
@@ -10,9 +10,9 @@ import java.util.function.Supplier;
 
 public class AwaitHelper {
 
-    protected static final int WAIT_FOR_IN_SECONDS = 20;
-    protected static final int POLL_INTERVAL_MS = 200;
-    protected static final int POLL_DELAY_MS = 20;
+    protected static final int WAIT_FOR_IN_SECONDS = 30;
+    protected static final int POLL_INTERVAL_MS = 300;
+    protected static final int POLL_DELAY_MS = 30;
 
     public static <T> T waitFor(Supplier<T> supplier) {
         var dataToReturn = new AtomicReference<T>();
