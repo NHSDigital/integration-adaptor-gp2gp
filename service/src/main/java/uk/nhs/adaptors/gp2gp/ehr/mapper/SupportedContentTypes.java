85591175b5057b5655c8d044c402cc48855df327
msolecka <m.solecka@kainos.com>
2021-06-02 17:35:01 +0200

    NIAD-1189: Move supported-content-types.txt to other dir and change the way of loading supported content types due to problem with bean initialization

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/SupportedContentTypes.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/SupportedContentTypes.java
@@ -1,38 +1,45 @@
 package uk.nhs.adaptors.gp2gp.ehr.mapper;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import uk.nhs.adaptors.gp2gp.ehr.exception.EhrMapperException;
 
 @Component
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class SupportedContentTypes {
-    private static final String SUPPORTED_CONTENT_TYPES_PATH = "attachments/supported-content-types.txt";
-
-    private final List<String> supportedTypes;
-
-    public SupportedContentTypes() {
-        try {
-            URL resource = SupportedContentTypes.class.getClassLoader().getResource(SUPPORTED_CONTENT_TYPES_PATH);
-            if (resource != null) {
-                supportedTypes = Files.readAllLines(Paths.get(resource.toURI()));
-            } else {
-                throw new EhrMapperException("File does not exist. Unable to load supported content-types from location: "
-                    + SUPPORTED_CONTENT_TYPES_PATH);
-            }
-        } catch (IOException | URISyntaxException e) {
-            throw new EhrMapperException("Unable to load supported content-types from location: " + SUPPORTED_CONTENT_TYPES_PATH, e);
-        }
-    }
+    private static final String SUPPORTED_CONTENT_TYPES_PATH = "supported-content-types.txt";
+    private static final List<String> SUPPORTED_CONTENT_TYPES = loadSupportedContentTypes();
 
     public boolean isContentTypeSupported(String attachmentContentType) {
-        return !CollectionUtils.isEmpty(supportedTypes) && supportedTypes.contains(attachmentContentType);
+        return !CollectionUtils.isEmpty(SUPPORTED_CONTENT_TYPES) && SUPPORTED_CONTENT_TYPES.contains(attachmentContentType);
+    }
+
+    @SneakyThrows
+    @SuppressFBWarnings(value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
+        justification = "https://github.com/spotbugs/spotbugs/issues/1338")
+    private static List<String> loadSupportedContentTypes() {
+        try (InputStream is = SupportedContentTypes.class.getClassLoader().getResourceAsStream(SUPPORTED_CONTENT_TYPES_PATH);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(is, UTF_8))) {
+            return reader.lines()
+                .filter(StringUtils::isNotBlank)
+                .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new EhrMapperException("Unable to load supported content-types from location: " + SUPPORTED_CONTENT_TYPES_PATH, e);
+        }
     }
 }
