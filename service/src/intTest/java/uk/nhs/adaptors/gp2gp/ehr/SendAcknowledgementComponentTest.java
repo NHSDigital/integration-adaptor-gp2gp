2a1119a8d32768fc9e5c5274056e556c2d1e9d3b
Broom94 <42891767+Broom94@users.noreply.github.com>
2021-06-04 13:30:04 +0100

    Update service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/SendAcknowledgementComponentTest.java
    Co-authored-by: Matt Dooner <50910155+mattd-kainos@users.noreply.github.com>

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/SendAcknowledgementComponentTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/SendAcknowledgementComponentTest.java
@@ -43,7 +43,7 @@ public class SendAcknowledgementComponentTest {
     private EhrExtractStatusRepository ehrExtractStatusRepository;
 
     @Test
-    public void When_AcknowledgementTaskRunsTwice_Expect_DatabaseOverwritesEhrExtarctStatus() {
+    public void When_AcknowledgementTaskRunsTwice_Expect_DatabaseOverwritesEhrExtractStatus() {
         var ehrExtractStatus = EhrExtractStatusTestUtils.prepareEhrExtractStatus();
         ehrExtractStatusRepository.save(ehrExtractStatus);
 
