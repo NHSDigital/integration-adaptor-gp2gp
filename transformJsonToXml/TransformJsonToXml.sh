dd1f59ec4a8909f04f3ffbbcc2ee0c2ce4dba68c
AlanSimpson1723 <56081521+AlanSimpson1723@users.noreply.github.com>
2021-06-08 16:07:41 +0100

    removed the need for env. variables

--- transformJsonToXml/TransformJsonToXml.sh
+++ transformJsonToXml/TransformJsonToXml.sh
@@ -1,4 +1,2 @@
-cd "../service/src/main/java/uk/nhs/adaptors/gp2gp/transformjsontoxmltool/"
-export JSON_TO_XML_START_TOOL="True"
-cd "../../../../../../../../"
+cd "../service/"
 ./gradlew runSimple
