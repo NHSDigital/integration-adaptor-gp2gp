4f3dedc0d655668832dde28619a11266d7b152bc
Leszek Gonczar <gonczar@gmail.com>
2021-06-15 14:48:41 +0200

    NIAD-1166 Create a dispatcher for 'Send NACK message' task, along with related classes

--- service/src/main/java/uk/nhs/adaptors/gp2gp/common/task/TaskType.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/common/task/TaskType.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import uk.nhs.adaptors.gp2gp.ehr.SendAcknowledgementTaskDefinition;
 import uk.nhs.adaptors.gp2gp.ehr.SendDocumentTaskDefinition;
 import uk.nhs.adaptors.gp2gp.ehr.SendEhrExtractCoreTaskDefinition;
+import uk.nhs.adaptors.gp2gp.ehr.SendNegativeAcknowledgementTaskDefinition;
 import uk.nhs.adaptors.gp2gp.gpc.GetGpcDocumentReferencesTaskDefinition;
 import uk.nhs.adaptors.gp2gp.gpc.GetGpcDocumentTaskDefinition;
 import uk.nhs.adaptors.gp2gp.gpc.GetGpcStructuredTaskDefinition;
@@ -17,7 +18,8 @@ public enum TaskType {
     GPC_FIND_DOCUMENTS(GetGpcDocumentReferencesTaskDefinition.class),
     SEND_EHR_EXTRACT_CORE(SendEhrExtractCoreTaskDefinition.class),
     SEND_EHR_CONTINUE(SendDocumentTaskDefinition.class),
-    SEND_ACKNOWLEDGEMENT(SendAcknowledgementTaskDefinition.class);
+    SEND_ACKNOWLEDGEMENT(SendAcknowledgementTaskDefinition.class),
+    SEND_NEGATIVE_ACKNOWLEDGEMENT(SendNegativeAcknowledgementTaskDefinition.class);
 
     private final Class<? extends TaskDefinition> classOfTaskDefinition;
 
