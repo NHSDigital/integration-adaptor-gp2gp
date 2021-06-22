55ab632390a7df532c4ad461f065ae040e5e1ef2
Matthew O'Reilly <matthewdoreilly@users.noreply.github.com>
2021-06-22 10:43:20 +0100

    NIAD-1031: Make the names of files uploaded to storage (Local HashMap, AWS, Azure) unique, to prevent accidental collisions/overwriting (#265)

---
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/gpc/GpcFilenameUtils.java
@@ -0,0 +1,15 @@
+package uk.nhs.adaptors.gp2gp.gpc;
+
+public class GpcFilenameUtils {
+    private static final String PATH_SEPARATOR = "/";
+    private static final String JSON_EXTENSION = ".json";
+    private static final String GPC_STRUCTURED_FILE_EXTENSION = "_gpc_structured" + JSON_EXTENSION;
+
+    public static String generateStructuredRecordFilename(String conversationId) {
+        return conversationId.concat(PATH_SEPARATOR).concat(conversationId).concat(GPC_STRUCTURED_FILE_EXTENSION);
+    }
+
+    public static String generateDocumentFilename(String conversationId, String documentId) {
+        return conversationId.concat(PATH_SEPARATOR).concat(documentId).concat(JSON_EXTENSION);
+    }
+}
