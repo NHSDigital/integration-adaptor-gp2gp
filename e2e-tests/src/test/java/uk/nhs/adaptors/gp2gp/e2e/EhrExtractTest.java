5ca2fc2a5c831d78cf5bac478bfb535ba87037d0
ElliotB-96 <elliotb@kainos.com>
2021-07-08 08:23:20 +0100

    Updated e2e test await function

--- e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/EhrExtractTest.java
+++ e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/EhrExtractTest.java
@@ -117,8 +117,7 @@ public class EhrExtractTest {
     }
 
     private void assertMultipleDocsSent(String conversationId) {
-        var ehrExtractStatus = waitFor(() -> Mongo.findEhrExtractStatus(conversationId));
-        var gpcAccessDocument = (Document) ehrExtractStatus.get(GPC_ACCESS_DOCUMENT);
+        var gpcAccessDocument = (Document) waitFor(() -> Mongo.findEhrExtractStatus(conversationId)).get(GPC_ACCESS_DOCUMENT);
         var document = getFirstDocumentIfItHasObjectNameOrElseNull(gpcAccessDocument);
         var sentToMhs = (Document) document.get(SENT_TO_MHS);
         var messageIds = sentToMhs.get(MESSAGE_ID, Collections.emptyList());
