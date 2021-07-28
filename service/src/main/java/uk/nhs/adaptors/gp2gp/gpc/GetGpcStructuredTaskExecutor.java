db8fbad8b6bc2dcb14565d14c152ac31f59f7bb6
ElliotB-96 <elliotb@kainos.com>
2021-07-28 13:39:33 +0100

    Tidy up for GPC 1.6 uplift

--- service/src/main/java/uk/nhs/adaptors/gp2gp/gpc/GetGpcStructuredTaskExecutor.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/gpc/GetGpcStructuredTaskExecutor.java
@@ -6,10 +6,6 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.dstu3.model.DocumentReference;
-import org.hl7.fhir.dstu3.model.IdType;
-import org.hl7.fhir.dstu3.model.Patient;
-import org.hl7.fhir.dstu3.model.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.nhs.adaptors.gp2gp.common.service.FhirParseService;
@@ -21,11 +17,9 @@ import uk.nhs.adaptors.gp2gp.common.task.TaskExecutor;
 import uk.nhs.adaptors.gp2gp.ehr.EhrExtractStatusService;
 import uk.nhs.adaptors.gp2gp.ehr.mapper.MessageContext;
 import uk.nhs.adaptors.gp2gp.ehr.model.EhrExtractStatus;
-import uk.nhs.adaptors.gp2gp.ehr.utils.ResourceExtractor;
 import uk.nhs.adaptors.gp2gp.mhs.model.OutboundMessage;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -66,7 +60,8 @@ public class GetGpcStructuredTaskExecutor implements TaskExecutor<GetGpcStructur
                 .filter(documentMetadata -> StringUtils.isBlank(documentMetadata.getUrl()))
                 .collect(Collectors.toList());
             if (!documentReferencesWithoutUrl.isEmpty()) {
-                LOGGER.warn("Following DocumentReference resources does not have any Attachments with URL: {}", documentReferencesWithoutUrl);
+                LOGGER.warn("Following DocumentReference resources does not have any Attachments with URL: {}",
+                    documentReferencesWithoutUrl);
             }
 
             externalAttachments = externalAttachments.stream()
@@ -110,47 +105,14 @@ public class GetGpcStructuredTaskExecutor implements TaskExecutor<GetGpcStructur
         detectTranslationCompleteService.beginSendingCompleteExtract(ehrExtractStatus);
     }
 
-    private Bundle  getStructuredRecord(GetGpcStructuredTaskDefinition structuredTaskDefinition) {
-        var structuredRecord = fhirParseService.parseResource(gpcClient.getStructuredRecord(structuredTaskDefinition), Bundle.class);
-
-
-        //TODO whole block below might not be needed in GPC 1.5.1
-//        getPatientId(structuredTaskDefinition).ifPresent(patientId -> {
-//            ehrExtractStatusService.updateEhrExtractStatusAccessDocumentPatientId(structuredTaskDefinition, patientId);
-//            // calling getDocumentReferencesBundle because currently getStructuredRecord doesn't have document references
-//            var documentReferences = getDocumentReferencesBundle(structuredTaskDefinition, patientId);
-//
-//            ResourceExtractor.extractResourcesByType(documentReferences, DocumentReference.class)
-//                .map(resource -> new Bundle.BundleEntryComponent().setResource(resource))
-//                .forEach(bundleEntry -> structuredRecord.getEntry().add(bundleEntry));
-//            structuredRecord.setTotal(structuredRecord.getEntry().size());
-//        });
-        return structuredRecord;
+    private Bundle getStructuredRecord(GetGpcStructuredTaskDefinition structuredTaskDefinition) {
+        return fhirParseService.parseResource(gpcClient.getStructuredRecord(structuredTaskDefinition), Bundle.class);
     }
 
     private void queueGetDocumentsTask(TaskDefinition taskDefinition, List<OutboundMessage.ExternalAttachment> externalAttachments) {
         externalAttachments.stream()
             .map(externalAttachment -> buildGetDocumentTask(taskDefinition, externalAttachment))
             .forEach(taskDispatcher::createTask);
-    }
-
-    private Optional<String> getPatientId(GetGpcStructuredTaskDefinition taskDefinition) {
-        var response = gpcClient.getPatientRecord(taskDefinition);
-
-        var fhirBundle = fhirParseService.parseResource(response, Bundle.class);
-
-        return ResourceExtractor.extractResourcesByType(fhirBundle, Patient.class)
-            .map(Resource::getIdElement)
-            .map(IdType::getIdPart)
-            .reduce((a, b) -> {
-                throw new IllegalStateException("There is more than 1 Patient resource in Structured Record Bundle");
-            });
-    }
-
-    private Bundle getDocumentReferencesBundle(GetGpcStructuredTaskDefinition taskDefinition, String patientId) {
-        var response = gpcClient.getDocumentReferences(taskDefinition, patientId);
-
-        return fhirParseService.parseResource(response, Bundle.class);
     }
 
     private GetGpcDocumentTaskDefinition buildGetDocumentTask(TaskDefinition taskDefinition,
