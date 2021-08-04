a1294c882bf23a5560ecc714461256dec3a5a26d
ElliotB-96 <elliotb@kainos.com>
2021-08-04 08:50:02 +0100

    Updated await timer to 5mins

--- e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/AwaitHelper.java
+++ e2e-tests/src/test/java/uk/nhs/adaptors/gp2gp/e2e/AwaitHelper.java
@@ -10,7 +10,7 @@ import java.util.function.Supplier;
 
 public class AwaitHelper {
 
-    protected static final int WAIT_FOR_IN_SECONDS = 120;
+    protected static final int WAIT_FOR_IN_SECONDS = 300;
     protected static final int POLL_INTERVAL_MS = 300;
     protected static final int POLL_DELAY_MS = 30;
 
