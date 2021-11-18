d61de202af27eb63646d03f1047534823293cd7b
bchotkowski <bartosz.chotkowski@kainos.com>
2021-11-18 16:29:12 +0100

    Created a class to handle mapping data with mustache template
    -> Using AbsentAttachment class with builder for now (draft), might be changed to AbsentAttachmentParameters soon
    ->  Using absent_attachment_template.mustache as template to fill data

---
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/AbsentAttachmentFileMapper.java
@@ -0,0 +1,37 @@
+package uk.nhs.adaptors.gp2gp.ehr.mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.mustachejava.Mustache;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import uk.nhs.adaptors.gp2gp.ehr.utils.TemplateUtils;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AbsentAttachmentFileMapper {
+
+    private static final Mustache ABSENT_ATTACHMENT_TEMPLATE = TemplateUtils.loadTemplate("absent_attachment_template.mustache");
+
+    public static String mapDataToAbsentAttachment() {
+        var absentAttachment = AbsentAttachment.builder()
+            .title("Missing file") //pass the value of content.attachment.title got from Emis
+            .sourcePracticeOrganisationCode("A81001")  //transaction or wrapper layer
+            .ebXMLConversationId("6F4ECB85-43E4-41CD-A1BB-330C06D8FC99") //transaction or wrapper layer
+            .build();
+        return TemplateUtils.fillTemplate(ABSENT_ATTACHMENT_TEMPLATE, absentAttachment);
+    }
+
+
+    //TODO: Create a uk.nhs.adaptors.gp2gp.ehr.mapper.parameters.AbsentAttachmentParameters class if needed
+    // This one's only draft
+    @Builder
+    private static class AbsentAttachment {
+        private String title; //content.attachment.title
+        private String sourcePracticeOrganisationCode;
+        private String ebXMLConversationId;
+    }
+
+}
