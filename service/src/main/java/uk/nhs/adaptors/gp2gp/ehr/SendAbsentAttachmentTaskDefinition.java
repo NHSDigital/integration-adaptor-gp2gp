abf25acc37c0a1d7aa6e85ac7137205e8be23a01
bchotkowski <bartosz.chotkowski@kainos.com>
2021-12-06 13:10:45 +0100

    Added a default constructor and annotation
    InvalidDefinitionException resolving

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/SendAbsentAttachmentTaskDefinition.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/SendAbsentAttachmentTaskDefinition.java
@@ -5,12 +5,19 @@ import static uk.nhs.adaptors.gp2gp.common.task.TaskType.SEND_ABSENT_ATTACHMENT;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 import uk.nhs.adaptors.gp2gp.common.task.TaskType;
 
+@Jacksonized
 @Getter
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 public class SendAbsentAttachmentTaskDefinition extends DocumentTaskDefinition {
+
+    public SendAbsentAttachmentTaskDefinition(DocumentTaskDefinitionBuilder<?, ?> b, String title) {
+        super(b);
+        this.title = title;
+    }
 
     /**
      * content.attachment.title of DocumentReference qualified to be an AbsentAttachment
