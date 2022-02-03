23ba5a4e2c53f3996f2952fac7448eedc72d3a62
Barry Bell <gavin.bell@kainos.com>
2022-02-03 16:09:00 +0000

    Small fix

--- service/src/test/java/uk/nhs/adaptors/gp2gp/mhs/model/AttachmentDescriptionTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/mhs/model/AttachmentDescriptionTest.java
@@ -11,7 +11,7 @@ class AttachmentDescriptionTest {
     @Test
     void When_BuildingAttachmentDescriptionUsingMandatoryParameters_Expect_ProperDescriptionIsCreated() {
         var result = OutboundMessage.AttachmentDescription.builder()
-            .fileName("\"some_file_name\"")
+            .fileName("some_file_name")
             .contentType("some_content_type")
             .compressed(false)
             .largeAttachment(false)
@@ -20,7 +20,7 @@ class AttachmentDescriptionTest {
             .toString();
 
         assertThat(result).isEqualTo("\n"
-            + "                Filename=some_file_name\n"
+            + "                Filename=\"some_file_name\"\n"
             + "                ContentType=some_content_type\n"
             + "                Compressed=No\n"
             + "                LargeAttachment=No\n"
@@ -31,7 +31,7 @@ class AttachmentDescriptionTest {
     @Test
     void When_BuildingAttachmentDescriptionUsingAllParameters_Expect_ProperDescriptionIsCreated() {
         var result = OutboundMessage.AttachmentDescription.builder()
-            .fileName("\"some_other_file_name\"")
+            .fileName("some_other_file_name")
             .contentType("some_other_content_type")
             .compressed(true)
             .largeAttachment(true)
@@ -42,7 +42,7 @@ class AttachmentDescriptionTest {
             .toString();
 
         assertThat(result).isEqualTo("\n"
-            + "                Filename=some_other_file_name\n"
+            + "                Filename=\"some_other_file_name\"\n"
             + "                ContentType=some_other_content_type\n"
             + "                Compressed=Yes\n"
             + "                LargeAttachment=Yes\n"
