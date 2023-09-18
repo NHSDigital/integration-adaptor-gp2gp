98e698e3596f771aba446a89a715ad898f89c6d3
hospel <ole4ryb@gmail.com>
2023-09-18 14:25:56 +0100

    base64 adjustments for test

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/SendDocumentTaskExecutor.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/SendDocumentTaskExecutor.java
@@ -85,7 +85,7 @@ public class SendDocumentTaskExecutor implements TaskExecutor<SendDocumentTaskDe
                         .contentType(taskDefinition.getDocumentContentType())
                         .compressed(false) //const
                         .largeAttachment(false) // const - chunks are not large attachments themself
-                        .originalBase64(true) //const
+                        .originalBase64(false) //const
                         .build()
                         .toString())
                     .messageId(messageId)
