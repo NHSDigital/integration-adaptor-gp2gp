02bb9b046bed8bd697ce0b258a584b3599deddc3
BenHession <bshession@gmail.com>
2023-11-13 14:18:05 +0000

    guard against null pointer

--- service/src/main/java/uk/nhs/adaptors/gp2gp/common/amqp/JmsListenerErrorHandler.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/common/amqp/JmsListenerErrorHandler.java
@@ -18,12 +18,27 @@ public class JmsListenerErrorHandler implements ErrorHandler {
         MhsConnectionException.class, "Unable to connect to MHS Outbound"
     );
 
+    /**
+     * Increment the delivery count for all exceptions, except for RETRYABLE_EXCEPTION_MESSAGES which are retried indefinitely.
+     * See <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/jms/listener/AbstractMessageListenerContainer.html#class-description">AbstractMessageListenerContainer</a>
+     * for documentation.
+     */
     @Override
     public void handleError(Throwable t) {
 
-        Throwable cause = t.getCause();
-        LOGGER.warn("Caught Error cause of type: [{}], with message: [{}]", cause.getClass().toString(), cause.getMessage());
+        LOGGER.error("Handling JMS message error due to [{}] with message [{}]", t.getClass(), t.getMessage());
+        t.printStackTrace();
 
+        Throwable cause = t.getCause();
+        if (cause == null) {
+            return;
+        }
+
+        Class<? extends Throwable> classOfCause = cause.getClass();
+        LOGGER.error("Caught Error cause of type: [{}], with message: [{}]", classOfCause.toString(), cause.getMessage());
+
+        // Retry these specific exceptions continuously until they stop happening.
+        // These retries will happen until the associated transfer times out.
         if (RETRYABLE_EXCEPTION_MESSAGES.containsKey(cause.getClass())) {
             throw new RuntimeException(RETRYABLE_EXCEPTION_MESSAGES.get(cause.getClass()));
         }
