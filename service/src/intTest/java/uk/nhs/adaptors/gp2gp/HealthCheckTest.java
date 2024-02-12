f28db276a824c08af1a88e267613504ff815f1bd
Adrian Clay <adrian.clay1@nhs.net>
2024-02-12 09:48:51 +0000

    Fix deprecation warnings (#595)
    * Move LocalServerPort to new namespace as of Spring Boot 2.7
    * Gradle `main` property has been deprecated in favour of `mainClass`

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/HealthCheckTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/HealthCheckTest.java
@@ -6,7 +6,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.reactive.function.client.WebClient;
