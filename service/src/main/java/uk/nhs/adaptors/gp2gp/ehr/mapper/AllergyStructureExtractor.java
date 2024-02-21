0d0d78110bc907ae496bf46deb27f4535e8fced5
Alex-Nita <127960028+Alex-Nita@users.noreply.github.com>
2024-02-21 09:30:09 +0000

    Restructured availability time value mappings (#602)
    * Restructured availability time value mappings
    * Changed source value for availabilityTime from onsetDate to assertedDate
    * Corrected test data.
    * Removed Line Break

--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/AllergyStructureExtractor.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/mapper/AllergyStructureExtractor.java
@@ -59,6 +59,13 @@ public class AllergyStructureExtractor {
         return StringUtils.EMPTY;
     }
 
+    public static String extractAssertedDate(AllergyIntolerance allergyIntolerance) {
+        if (allergyIntolerance.hasAssertedDateElement() && allergyIntolerance.getAssertedDateElement().hasValue()) {
+            return toHl7Format(allergyIntolerance.getAssertedDateElement());
+        }
+        return StringUtils.EMPTY;
+    }
+
     public static String extractReaction(AllergyIntolerance.AllergyIntoleranceReactionComponent reactionComponent,
         AtomicInteger reactionCount) {
         String reaction = String.format(REACTION, reactionCount.getAndIncrement());
