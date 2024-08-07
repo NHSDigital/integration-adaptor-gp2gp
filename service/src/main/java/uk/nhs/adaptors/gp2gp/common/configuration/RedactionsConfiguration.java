2dc7ae270e7706979aed83ddae61dc7c2fc6e037
Adrian Clay <adrian@madetech.com>
2024-08-07 20:14:23 +0100

    Refactor: Remove the duplicated constants to simplify

--- service/src/main/java/uk/nhs/adaptors/gp2gp/common/configuration/RedactionsConfiguration.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/common/configuration/RedactionsConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class RedactionsConfiguration {
-    public static final String EHR_EXTRACT_INTERACTION_ID = "RCMR_IN030000UK06";
-    public static final String EHR_EXTRACT_INTERACTION_ID_WITH_REDACTIONS = "RCMR_IN030000UK07";
     private boolean redactionsEnabled;
 
     @Value("${gp2gp.redactions-enabled}")
@@ -18,8 +16,8 @@ class RedactionsConfiguration {
     @Bean
     RedactionsContext redactionsContext() {
         final String ehrExtractInteractionId = redactionsEnabled
-            ? EHR_EXTRACT_INTERACTION_ID_WITH_REDACTIONS
-            : EHR_EXTRACT_INTERACTION_ID;
+            ? RedactionsContext.REDACTION_INTERACTION_ID
+            : RedactionsContext.NON_REDACTION_INTERACTION_ID;
 
         return new RedactionsContext(ehrExtractInteractionId);
     }
