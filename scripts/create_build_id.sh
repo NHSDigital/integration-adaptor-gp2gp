1094105538741de40c1d26c9950970e8f331682f
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-09-16 17:04:33 +0100

    [NIAD-3173-POST-TESTING-FIXES] Fix image tagging mechanism (#874)

---
+++ scripts/create_build_id.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clean_tag_element() {
+    local tag_element="$1"
+    echo "${tag_element//\//-}"
+}
+
+generate_tag() {
+    local clean_branch_name=$(clean_tag_element "$1")
+    local clean_build_id=$(clean_tag_element "$2")
+    local short_git_hash=$(${!3}:0:7)
+
+    local tag="${clean_branch_name}-${clean_build_id}-${short_git_hash}"
+
+    echo "$tag"
+}
+
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 branch_name build_id git_hash"
+    exit 1
+fi
+
+branch_name="$1"
+build_id="$2"
+git_hash="$3"
+
+generate_tag "$branch_name" "$build_id" "$git_hash"
