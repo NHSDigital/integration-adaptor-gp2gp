80a87d0f8dc31ecb545088a349b5311de8b6671c
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-09-17 16:12:50 +0100

    [NIAD-3144-FIXES] Remove unnecessary tests

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/diagnosticreport/DiagnosticReportMapperTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/mapper/diagnosticreport/DiagnosticReportMapperTest.java
@@ -187,27 +187,6 @@ class DiagnosticReportMapperTest {
                 () -> assertThat(actualXml).containsIgnoringWhitespaces(NOPAT_HL7_CONFIDENTIALITY_CODE)
             );
         }
-
-        @Test
-        void When_DiagnosticReport_With_FilingComment_Expect_ConfidentialityCodeNotPresentWithinUserCommentNarrativeStatement() {
-            final DiagnosticReport diagnosticReport = getDiagnosticReportResourceFromJson("diagnostic-report-with-one-result.json");
-            final Bundle bundle = getBundleResourceFromJson("fhir_bundle_filing_comment.json");
-
-            when(confidentialityService.generateConfidentialityCode(bundle.getEntry().getFirst().getResource()))
-                .thenReturn(Optional.empty());
-            when(messageContext.getInputBundleHolder())
-                .thenReturn(new InputBundle(bundle));
-
-            // The presence of a status withing DiagnosticReport
-            // generates an additional NarrativeStatement.
-            diagnosticReport.setStatus(null);
-
-            final String actualXml = mapper.mapDiagnosticReportToCompoundStatement(diagnosticReport);
-
-            assertAll(
-                () -> assertThat(actualXml).doesNotContain(NOPAT_HL7_CONFIDENTIALITY_CODE)
-            );
-        }
     }
 
     private Bundle getBundleResourceFromJson(String filename) {
