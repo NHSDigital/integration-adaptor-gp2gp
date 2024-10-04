6da6740f40c8d8a07a9091d90889a87b9768c339
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-10-04 16:00:35 +0100

    checkstyle

--- service/src/main/java/uk/nhs/adaptors/gp2gp/common/service/ConfidentialityService.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/common/service/ConfidentialityService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.nhs.adaptors.gp2gp.common.configuration.RedactionsContext;
 
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
