a67631ad7e20e00a59bc809b2724a3de32ae27bd
MartinWheelerMT <martin.wheeler@madetech.com>
2024-10-09 16:19:36 +0100

    Fix issue with nullability checking in ResourceReader
    The method `readAsString` was using an input string which was marked as @Nullable, in method parameter marked as @NotNull.  Although this should never happen, based on the code which uses it, but we need to address the potential nullPointerException

--- mock-mhs-adaptor/src/main/java/uk/nhs/adaptors/mockmhsservice/common/ResourceReader.java
+++ mock-mhs-adaptor/src/main/java/uk/nhs/adaptors/mockmhsservice/common/ResourceReader.java
@@ -2,6 +2,7 @@ package uk.nhs.adaptors.mockmhsservice.common;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 import lombok.SneakyThrows;
 
@@ -9,7 +10,7 @@ public class ResourceReader {
     @SneakyThrows
     public static String readAsString(String resourceName) {
         try (InputStream inputStream = ResourceReader.class.getResourceAsStream(String.format("/%s",resourceName))) {
-            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            return new String(Objects.requireNonNull(inputStream).readAllBytes(), StandardCharsets.UTF_8);
         }
     }
 }
