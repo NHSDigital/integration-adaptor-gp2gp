b8e01735cf402fb533ef33a66be5fe556f13262c
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-11-12 14:33:10 +0000

    marking EhrExtractStatus as failed in a test scenario

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendControllerTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendControllerTest.java
@@ -13,12 +13,24 @@ import uk.nhs.adaptors.gp2gp.ehr.model.EhrExtractStatus;
 import uk.nhs.adaptors.gp2gp.testcontainers.ActiveMQExtension;
 import uk.nhs.adaptors.gp2gp.testcontainers.MongoDBExtension;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.nhs.adaptors.gp2gp.ehr.EhrStatusConstants.INCUMBENT_NACK_CODE;
+import static uk.nhs.adaptors.gp2gp.ehr.EhrStatusConstants.INCUMBENT_NACK_DISPLAY;
+
 
 @SpringBootTest
 @DirtiesContext
 @ExtendWith({SpringExtension.class, MongoDBExtension.class, ActiveMQExtension.class, MockitoExtension.class})
 public class EhrResendControllerTest {
+
+    public static final Instant NOW = Instant.parse("2024-01-01T10:00:00Z");
+    private static final Instant FIVE_DAYS_AGO = NOW.minus(Duration.ofDays(5));
+
     @Autowired
     private EhrExtractStatusRepository ehrExtractStatusRepository;
     @Autowired
@@ -26,9 +38,22 @@ public class EhrResendControllerTest {
 
     @Test
     public void When_AnEhrExtractHasFailed_Expect_RespondsWith202() {
+
+        String ehrMessageRef = generateRandomUppercaseUUID();
         var ehrExtractStatus = new EhrExtractStatus();
         ehrExtractStatus.setConversationId("123-456");
-        // TODO: Mark ehrExtractStatus as failed.
+        ehrExtractStatus.setEhrReceivedAcknowledgement(EhrExtractStatus.EhrReceivedAcknowledgement.builder()
+                                        .conversationClosed(FIVE_DAYS_AGO)
+                                        .errors(List.of(
+                                            EhrExtractStatus.EhrReceivedAcknowledgement.ErrorDetails.builder()
+                                                .code(INCUMBENT_NACK_CODE)
+                                                .display(INCUMBENT_NACK_DISPLAY)
+                                                .build()))
+                                        .messageRef(ehrMessageRef)
+                                        .received(FIVE_DAYS_AGO)
+                                        .rootId(generateRandomUppercaseUUID())
+                                        .build());
+
         ehrExtractStatusRepository.save(ehrExtractStatus);
 
         var response = ehrResendController.scheduleEhrExtractResend("123-456");
@@ -45,4 +70,10 @@ public class EhrResendControllerTest {
         // TODO: Add more detail to OperationOutcome
         assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(new OperationOutcome());
     }
+
+    private String generateRandomUppercaseUUID() {
+        return UUID.randomUUID().toString().toUpperCase();
+    }
+
+
 }
