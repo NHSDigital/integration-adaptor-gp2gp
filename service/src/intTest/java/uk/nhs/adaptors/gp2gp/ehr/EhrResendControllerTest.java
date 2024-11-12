49dd7e8060e73f9dbe2f7510c16b1f6a92dac1c3
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-11-12 15:48:19 +0000

    the endpoint returns operationOutcome populated with more information

--- service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendControllerTest.java
+++ service/src/intTest/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendControllerTest.java
@@ -1,6 +1,10 @@
 package uk.nhs.adaptors.gp2gp.ehr;
 
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.Meta;
 import org.hl7.fhir.dstu3.model.OperationOutcome;
+import org.hl7.fhir.dstu3.model.UriType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,13 +19,14 @@ import uk.nhs.adaptors.gp2gp.testcontainers.MongoDBExtension;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hl7.fhir.dstu3.model.OperationOutcome.IssueType;
 import static uk.nhs.adaptors.gp2gp.ehr.EhrStatusConstants.INCUMBENT_NACK_CODE;
 import static uk.nhs.adaptors.gp2gp.ehr.EhrStatusConstants.INCUMBENT_NACK_DISPLAY;
-
 
 @SpringBootTest
 @DirtiesContext
@@ -30,9 +35,12 @@ public class EhrResendControllerTest {
 
     public static final Instant NOW = Instant.parse("2024-01-01T10:00:00Z");
     private static final Instant FIVE_DAYS_AGO = NOW.minus(Duration.ofDays(5));
+    private static final String URI_TYPE = "https://fhir.nhs.uk/STU3/StructureDefinition/GPConnect-OperationOutcome-1";
+    private static final String CONVERSATION_ID = "123-456";
 
     @Autowired
     private EhrExtractStatusRepository ehrExtractStatusRepository;
+
     @Autowired
     private EhrResendController ehrResendController;
 
@@ -41,7 +49,8 @@ public class EhrResendControllerTest {
 
         String ehrMessageRef = generateRandomUppercaseUUID();
         var ehrExtractStatus = new EhrExtractStatus();
-        ehrExtractStatus.setConversationId("123-456");
+
+        ehrExtractStatus.setConversationId(CONVERSATION_ID);
         ehrExtractStatus.setEhrReceivedAcknowledgement(EhrExtractStatus.EhrReceivedAcknowledgement.builder()
                                         .conversationClosed(FIVE_DAYS_AGO)
                                         .errors(List.of(
@@ -56,7 +65,7 @@ public class EhrResendControllerTest {
 
         ehrExtractStatusRepository.save(ehrExtractStatus);
 
-        var response = ehrResendController.scheduleEhrExtractResend("123-456");
+        var response = ehrResendController.scheduleEhrExtractResend(CONVERSATION_ID);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.ACCEPTED);
         assertThat(response.getBody()).isNull();
@@ -64,15 +73,39 @@ public class EhrResendControllerTest {
 
     @Test
     public void When_AnEhrExtractDoesNotExist_Expect_RespondsWith404() {
-        var response = ehrResendController.scheduleEhrExtractResend("123-456");
+
+        var details = new CodeableConcept();
+        var codeableConceptCoding = new Coding();
+        codeableConceptCoding.setSystem("http://fhir.nhs.net/ValueSet/gpconnect-error-or-warning-code-1");
+        codeableConceptCoding.setCode("INVALID_IDENTIFIER_VALUE");
+        details.setCoding(List.of(codeableConceptCoding));
+        var diagnostics = "Provide a conversationId that exists and retry the operation";
+
+        var operationOutcome = createOperationOutcome(IssueType.VALUE, OperationOutcome.IssueSeverity.ERROR, details, diagnostics);
+
+        var response = ehrResendController.scheduleEhrExtractResend(CONVERSATION_ID);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        // TODO: Add more detail to OperationOutcome
-        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(new OperationOutcome());
+
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(operationOutcome);
     }
 
     private String generateRandomUppercaseUUID() {
         return UUID.randomUUID().toString().toUpperCase();
+    }
+
+    public static OperationOutcome createOperationOutcome(
+        OperationOutcome.IssueType type, OperationOutcome.IssueSeverity severity, CodeableConcept details, String diagnostics) {
+        var operationOutcome = new OperationOutcome();
+        Meta meta = new Meta();
+        meta.setProfile(Collections.singletonList(new UriType(URI_TYPE)));
+        operationOutcome.setMeta(meta);
+        operationOutcome.addIssue()
+            .setCode(type)
+            .setSeverity(severity)
+            .setDetails(details)
+            .setDiagnostics(diagnostics);
+        return operationOutcome;
     }
 
 
--- service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendController.java
+++ service/src/main/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendController.java
@@ -1,7 +1,11 @@
 package uk.nhs.adaptors.gp2gp.ehr;
 
 import lombok.AllArgsConstructor;
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.Meta;
 import org.hl7.fhir.dstu3.model.OperationOutcome;
+import org.hl7.fhir.dstu3.model.UriType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +15,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.nhs.adaptors.gp2gp.ehr.model.EhrExtractStatus;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping(path = "/ehr-resend")
 public class EhrResendController {
+
+    private static final String OPERATION_OUTCOME_URL = "https://fhir.nhs.uk/STU3/StructureDefinition/GPConnect-OperationOutcome-1";
+
     private EhrExtractStatusRepository ehrExtractStatusRepository;
 
     @PostMapping("/{conversationId}")
     public ResponseEntity<OperationOutcome> scheduleEhrExtractResend(@PathVariable String conversationId) {
         Optional<EhrExtractStatus> ehrExtractStatus = ehrExtractStatusRepository.findByConversationId(conversationId);
 
-        return ehrExtractStatus.map(e -> new ResponseEntity<OperationOutcome>(HttpStatus.ACCEPTED))
-                .orElse(new ResponseEntity<>(new OperationOutcome(), HttpStatus.NOT_FOUND));
+        if (ehrExtractStatus.isEmpty()) {
+            var details = new CodeableConcept();
+            var codeableConceptCoding = new Coding();
+            codeableConceptCoding.setSystem("http://fhir.nhs.net/ValueSet/gpconnect-error-or-warning-code-1");
+            codeableConceptCoding.setCode("INVALID_IDENTIFIER_VALUE");
+            details.setCoding(List.of(codeableConceptCoding));
+            var diagnostics = "Provide a conversationId that exists and retry the operation";
+
+            var operationOutcome = createOperationOutcome(OperationOutcome.IssueType.VALUE,
+                                                          OperationOutcome.IssueSeverity.ERROR,
+                                                          details,
+                                                          diagnostics);
+
+            return new ResponseEntity<>(operationOutcome, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+
+    }
+
+    public static OperationOutcome createOperationOutcome(
+        OperationOutcome.IssueType type, OperationOutcome.IssueSeverity severity, CodeableConcept details, String diagnostics) {
+        var operationOutcome = new OperationOutcome();
+        Meta meta = new Meta();
+        meta.setProfile(Collections.singletonList(new UriType(OPERATION_OUTCOME_URL)));
+        operationOutcome.setMeta(meta);
+        operationOutcome.addIssue()
+            .setCode(type)
+            .setSeverity(severity)
+            .setDetails(details)
+            .setDiagnostics(diagnostics);
+        return operationOutcome;
     }
 
 }
