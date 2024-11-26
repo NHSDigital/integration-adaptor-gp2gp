a14e9c35969eece275f256df72a7f7bcbf45e6d5
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-11-26 13:45:44 +0000

    adding tests to confirm the status of a transfer once it starts

--- service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendControllerTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/ehr/EhrResendControllerTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,8 @@ import uk.nhs.adaptors.gp2gp.common.service.RandomIdGeneratorService;
 import uk.nhs.adaptors.gp2gp.common.service.TimestampService;
 import uk.nhs.adaptors.gp2gp.common.task.TaskDispatcher;
 import uk.nhs.adaptors.gp2gp.ehr.model.EhrExtractStatus;
+import uk.nhs.adaptors.gp2gp.ehr.status.controller.EhrStatusController;
+import uk.nhs.adaptors.gp2gp.ehr.status.service.EhrStatusService;
 import uk.nhs.adaptors.gp2gp.gpc.GetGpcStructuredTaskDefinition;
 
 import java.time.Duration;
@@ -31,6 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.nhs.adaptors.gp2gp.ehr.status.model.MigrationStatus.IN_PROGRESS;
 
 @ExtendWith(MockitoExtension.class)
 class EhrResendControllerTest {
@@ -68,6 +72,9 @@ class EhrResendControllerTest {
 
     private EhrResendController ehrResendController;
 
+    private EhrStatusController ehrStatusController;
+    private EhrStatusService ehrStatusService;
+
 
     @BeforeEach
     void setUp() {
@@ -79,6 +86,8 @@ class EhrResendControllerTest {
                                                       randomIdGeneratorService,
                                                       timestampService,
                                                       fhirParseService);
+        ehrStatusService = new EhrStatusService(ehrExtractStatusRepository);
+        ehrStatusController = new EhrStatusController(ehrStatusService);
     }
 
     @Test
@@ -125,6 +134,40 @@ class EhrResendControllerTest {
             () -> assertNull(updatedEhrExtractStatus.get().getEhrReceivedAcknowledgement()),
             () -> assertNull(updatedEhrExtractStatus.get().getGpcAccessDocument())
         );
+    }
+
+    @Test
+    void When_ResendingTransfer_Expect_EhrStatusInProgressStatus() {
+
+        String ehrMessageRef = generateRandomUppercaseUUID();
+        var ehrExtractStatus = new EhrExtractStatus();
+
+        ehrExtractStatus.setConversationId(CONVERSATION_ID);
+        ehrExtractStatus.setEhrReceivedAcknowledgement(EhrExtractStatus.EhrReceivedAcknowledgement.builder()
+                                                           .conversationClosed(FIVE_DAYS_AGO)
+                                                           .errors(List.of(
+                                                               EhrExtractStatus.EhrReceivedAcknowledgement.ErrorDetails.builder()
+                                                                   .code(INCUMBENT_NACK_CODE)
+                                                                   .display(INCUMBENT_NACK_DISPLAY)
+                                                                   .build()))
+                                                           .messageRef(ehrMessageRef)
+                                                           .received(FIVE_DAYS_AGO)
+                                                           .rootId(generateRandomUppercaseUUID())
+                                                           .build());
+        ehrExtractStatus.setEhrRequest(EhrExtractStatus.EhrRequest.builder().nhsNumber(NHS_NUMBER).build());
+        ehrExtractStatus.setEhrExtractCorePending(EhrExtractStatus.EhrExtractCorePending.builder().build());
+        ehrExtractStatus.setEhrContinue(EhrExtractStatus.EhrContinue.builder().build());
+        ehrExtractStatus.setGpcAccessDocument(EhrExtractStatus.GpcAccessDocument.builder().build());
+
+        when(ehrExtractStatusRepository.findByConversationId(CONVERSATION_ID)).thenReturn(Optional.of(ehrExtractStatus));
+
+        Instant now = Instant.now().truncatedTo(ChronoUnit.MILLIS);
+        when(timestampService.now()).thenReturn(now);
+
+        ehrResendController.scheduleEhrExtractResend(CONVERSATION_ID);
+        var ehrStatus = ehrStatusController.getEhrStatus(CONVERSATION_ID);
+
+        assertEquals(IN_PROGRESS, ehrStatus.getBody().getMigrationStatus());
     }
 
     @Test
@@ -175,6 +218,7 @@ class EhrResendControllerTest {
 
         assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
         assertNull(response.getBody());
+
     }
 
     @Test
