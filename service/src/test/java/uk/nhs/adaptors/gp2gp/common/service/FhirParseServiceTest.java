150c1bbbe4f89195920b607d6fc94dcf59dc0f2e
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-11-27 16:15:16 +0000

    checkstyle

--- service/src/test/java/uk/nhs/adaptors/gp2gp/common/service/FhirParseServiceTest.java
+++ service/src/test/java/uk/nhs/adaptors/gp2gp/common/service/FhirParseServiceTest.java
@@ -1,6 +1,5 @@
 package uk.nhs.adaptors.gp2gp.common.service;
 
-import ca.uhn.fhir.parser.DataFormatException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
